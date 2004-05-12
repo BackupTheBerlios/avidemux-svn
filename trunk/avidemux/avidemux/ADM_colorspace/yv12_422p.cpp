a6b3a2f88035f4eaa1f3674dbf5b0280e3f8f9b0
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-05-12 17:44:26 +0000

    optimized YV12->422, added working mmx version

--- trunk/avidemux/avidemux/ADM_colorspace/yv12_422p.cpp
+++ trunk/avidemux/avidemux/ADM_colorspace/yv12_422p.cpp
@@ -29,78 +29,27 @@ static void YV12_422_Altivec( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h);
 #endif
 
 #ifdef USE_MMX
-/*
-		0-> y1
-		1-> y2
-		2->u
-       3->v
-		4-> out
-		5-> out 2
-
-	Process 16 bytes = 4 pix from y1 and 4 pix from y2
-*/
-#define LOAD_M "                                                       \n\
-# load y,u,v                                                 \n\
-pxor				%%mm7,%%mm7					\n\
-pxor				%%mm6,%%mm6					\n\
-movd 				(%2),%%mm0    # load 4u                 00000 u3 U2 U1 U0   \n\
-movd 				(%3),	%%mm1    # load 4v             00000 V3 V2 V1 V0      \n\
-punpcklbw   	%%mm0 , %%mm2 #                 00 u3 00 u2 00 u1 00 u0 \n\
-punpcklbw   	%%mm1 , %%mm3 #                 00 v3 00 v2 00 v1 00 V0 \n\
-# \n\
-punpcklwd 	%%mm7, %%mm2#                    00 00 00 u1 00 00 00 u0 \n\
-punpcklwd 	%%mm7, %%mm3 #                   00 00 00 v1 00 00 00 v0 \n\
-psllq      		$8 , %%mm4                     #       00 00 u1 00 00 00 u0 00 \n\
-psllq      		$24 , %%mm5                    #      v1 00 00 00 v0 00 00 00 \n\
-por					%%mm4,%%mm5                 #   v1 00 u1 00 v0 00 u0 00 \n\
-# now do y1 and y2 \n\
-movd				(%0), %%mm0 #				00 00 00 00 y3 y2 y1 y0 \n\
-movd				(%1), %%mm1 #				00 00 00 00 y3 y2 y1 y0     \n\
-punpcklbw   	%%mm0 , %%mm6 #                 00 y3 00 y2 00 y1 00 y0 \n\
-punpcklbw   	%%mm1 , %%mm7 #                 00 y3 00 y2 00 y1 00 y0 \n\
-por					%%mm5,%%mm6		#                 v1 y3 u1 y2 v0 y1 u0 y0 \n\
-por					%%mm5,%%mm7		#                 v1 y3 u1 y2 v0 y1 u0 y0  \n\
-movq      		%%mm6, (%4) # store \n\
-movq      		%%mm7, (%5) # store     \n\
-"
-
-
-
-
-
-
-void fastYV12_422_mmx( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h)
-{
-
-	uint8_t *u,*v;
-	
-		u=in+w*h;
-		v=u+(w*h>>2);
-
-	for(uint32_t y=h>>1;y>0;y--)
-	{
-			for(uint32_t x=w>>2; x>0;x--)
-			{
-
-               __asm__( ".align 8"
-										LOAD_M
-								  : : "r" (in), "r" (in+w), "r" (u), "r" (v),"r" (out), "r" (out+2*w));
-
-					in+=4;			
-					u+=2;
-					v+=2;
-					out+=8;
-				
-			}
-			// skip a yy line & out line
-			   in+=w;
-				out+=2*w;
-
-     }
-
-	__asm__ ("EMMS");
-}
+#include "../mpeg2enc/mmx.h"
+static void YV12_422_mmx( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h);
 #endif
+
+static void YV12_422_C( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h);
+
+
+void YV12_422( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h)
+{
+#ifdef HAVE_ALTIVEC 
+	if( !(w&7))
+		return YV12_422_Altivec(in,out,w,h);
+#endif	
+#if defined( USE_MMX) 
+	if(!(w&3))
+		return  YV12_422_mmx( in, out,  w, h);
+#endif
+	YV12_422_C(in,out,w,h);
+
+}
+
 
 // Convert yv12 planar to yuv422 i
 
@@ -141,15 +90,70 @@ uint32_t dx,dy;
 	
 	}
 }
-void YV12_422( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h)
-{
-#ifdef HAVE_ALTIVEC 
-	if( !(w&7))
-		return YV12_422_Altivec(in,out,w,h);
-#endif	
-	YV12_422_C(in,out,w,h);
 
+/*
+		0-> y1
+		1-> y2
+		2->u
+		3->v
+		4-> out
+		5-> out 2
+
+	Process 16 bytes = 4 pix from y1 and 4 pix from y2
+*/
+
+#ifdef USE_MMX
+#define LOAD_M "                                                       \n\
+# load y,u,v                                                 \n\
+movd 		(%2), %%mm0    	 		 # load 4u       00000 u3 U2 U1 U0   \n\
+movd 		(%3), %%mm1	    		 # load 4v       00000 V3 V2 V1 V0      \n\
+punpcklbw   	%%mm0,%%mm1 			 # U2V2U1V1U0V0\n\
+# now do y1 and y2 \n\
+movd		(%0),  %%mm2 			 # 00 00 00 00 y3 y2 y1 y0 \n\
+movd		(%1),  %%mm3 			 # 00 00 00 00 y3 y2 y1 y0     \n\
+punpcklbw   	%%mm1, %%mm2 			 # 00 y3 00 y2 00 y1 00 y0 \n\
+punpcklbw   	%%mm1, %%mm3 			 # 00 y3 00 y2 00 y1 00 y0 \n\
+movq      	%%mm2, (%4) 			 # store \n\
+movq      	%%mm3, (%5) 			 # store     \n\
+"
+void YV12_422_mmx( uint8_t *in, uint8_t *out, uint32_t w,uint32_t h)
+{
+
+	uint8_t *u,*v,*out2;
+	
+	
+		u=in+w*h;
+		v=u+(w*h>>2);
+		out2=out+2*w;
+
+	for(uint32_t y=h>>1;y>0;y--)
+	{
+		for(uint32_t x=w>>2; x>0;x--)
+		{
+
+			 
+			
+               __asm__( ".align 8"
+			LOAD_M
+			 : : "r" (in), "r" (in+w), "r" (u), "r" (v),"r" (out), "r" (out+2*w));
+
+			in+=4;			
+			u+=2;
+			v+=2;
+			out+=8;
+				
+			
+
+		}
+		// skip a yy line & out line
+		in+=w;
+		out+=2*w;	
+	}
+	__asm__ ("EMMS");
 }
+#endif
+
+
 #ifdef HAVE_ALTIVEC 
 
 #define LOAD_ALIGN(dest,src) \
@@ -184,20 +188,9 @@ vector unsigned char zero;
 			LOAD_ALIGN(vecu,u); // expand
 			LOAD_ALIGN(vecv,u); // expand
 			
-			// expand y's, keep 8 pixel
-			vecy=(VEC8)vec_mergeh(vecy,zero); // 0 Y0 0 Y1
-			vecy2=(VEC8)vec_mergeh(vecy2,zero);
-			
-			// expand u and v, keep 4 pixels
-			vecu=(VEC8)vec_mergeh(zero,vecu); // U0 0 U1
-			vecu=(VEC8)vec_mergeh((VEC16)zero,(VEC16)vecu);  //  U0 0 0 0 U1 00
-			vecv=(VEC8)vec_mergeh(zero,vecv); // V0 0 V1
-			vecv=(VEC8)vec_mergeh((VEC16)vecv,(VEC16)zero); // 0 0 V0 0 0 0 0 V1
-		
-			vecu=vec_or(vecu,vecv);	
-			vecy=vec_or(vecy,vecu);
-			vecy2=vec_or(vecy2,vecu);
-			
+			vecu=(VEC8)vec_mergeh(vecu,vecv);
+			vecy=(VEC8)vec_mergeh(vecy,vecu);
+			vecy2=(VEC8)vec_mergeh(vecy2,vecu);
 			// Store
 			vec_st(vecy,0,out); 
 			vec_st(vecy2,0,out2); 
