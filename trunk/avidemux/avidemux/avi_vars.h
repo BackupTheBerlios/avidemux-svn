390023d34b0f9288e0e1a490f1a2cf69b97b735d
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-07-31 17:04:11 +0000

    win32 has audio

--- trunk/avidemux/avidemux/avi_vars.h
+++ trunk/avidemux/avidemux/avi_vars.h
@@ -16,7 +16,11 @@
 //----------------------
 //----------------------
 //----------------------
-#if (defined( HAVE_LIBESD) && defined(HAVE_ESD_H)) || defined(OSS_SUPPORT) || defined (USE_ARTS) || defined(USE_SDL) || defined(CONFIG_DARWIN)
+#if (defined( HAVE_LIBESD) && defined(HAVE_ESD_H)) || \
+ defined(OSS_SUPPORT) || defined (USE_ARTS) || \
+  defined(USE_SDL) || defined(CONFIG_DARWIN) || \
+  defined(CYG_MANGLING)
+  
 #define HAVE_AUDIO
 #endif
 //----------------------
