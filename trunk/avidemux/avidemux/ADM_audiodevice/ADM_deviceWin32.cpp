c984e77ba19f3a154244b274fea8a250777a128a
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-07-31 16:54:41 +0000

    win32 audio

--- trunk/avidemux/avidemux/ADM_audiodevice/ADM_deviceWin32.cpp
+++ trunk/avidemux/avidemux/ADM_audiodevice/ADM_deviceWin32.cpp
@@ -10,7 +10,8 @@
 //
 //
 //
-// C++ Implementation: ADM_deviceSDL
+// C++ Implementation: ADM_deviceWin32
+// Use MM layer to output sound
 //
 // Description: 
 //
@@ -47,8 +48,27 @@
 #define MODULE_NAME  MODULE_ADEVICE
 #include "ADM_toolkit/ADM_debug.h"
 
-static HWAVEOUT  mydevice;
+#define BUCKET_SIZE 25000*4   // 1 sec
+#define NB_BUCKET 30
 
+typedef struct audioBucket
+{
+    int         fre;
+    WAVEHDR     hdr;
+}audioBucket;
+static void CALLBACK waveOutProc(
+  HWAVEOUT hwo,      
+  UINT uMsg,         
+  DWORD_PTR dwInstance,  
+  DWORD dwParam1,    
+  DWORD dwParam2     
+);
+static HWAVEOUT  myDevice;
+static MMRESULT    myError;
+static void 	handleMM(MMRESULT  err);
+static int      first=0;
+
+audioBucket buckets[NB_BUCKET];	
 //_______________________________________________
 //
 //_______________________________________________
@@ -62,6 +82,8 @@ win32AudioDevice::win32AudioDevice(void)
 {
 	printf("Creating SDL Audio device\n");
 	_inUse=0;
+	first=0;
+	memset(buckets,0,sizeof(buckets));
 	
 }
 //_______________________________________________
@@ -69,17 +91,23 @@ win32AudioDevice::win32AudioDevice(void)
 //_______________________________________________
 uint8_t  win32AudioDevice::stop(void) 
 {
-	
+    if(!_inUse) return 0;  	
 	printf("Closing win32 audio\n");
-	 waveOutPause(  mydevice);
-	 if(MMSYSERR_NOERROR!= waveOutClose(mydevice))
+	 waveOutPause(  myDevice);
+	 myError=waveOutClose(myDevice);
+	 first=0;
+	 if(MMSYSERR_NOERROR!= myError)
 	 {
 	 	printf("Close failed\n");
+	 	handleMM(myError);
 		return 0;
 	 }
 	
 	_inUse=0;    	
-
+	for(uint32_t i=0;i<NB_BUCKET;i++)
+	{
+		 delete []    buckets[i].hdr.lpData;
+	}
 	return 1;
 }
 
@@ -87,35 +115,62 @@ uint8_t  win32AudioDevice::stop(void)
 //
 //
 //_______________________________________________
-uint8_t sdlAudioDevice::init(uint32_t channel, uint32_t fq) 
+uint8_t win32AudioDevice::init(uint32_t channel, uint32_t fq) 
 {
 	
-SDL_AudioSpec spec,result;
-PWAVEFORMATEX wav;
+
+WAVEFORMATEX wav;
 		
 		printf("Opening Win32 Audio, fq=%d\n",fq);
+
 		if(_inUse) 
 		{
 			printf("Already running ?\n");
 			return 1; // ???
 		}
+		first=0;
+/*
+	WORD wFormatTag;
+	WORD nChannels;
+	DWORD nSamplesPerSec;
+	DWORD nAvgBytesPerSec;
+	WORD nBlockAlign;
+	WORD wBitsPerSample;
+	WORD cbSize;
+*/		
 		memset(&wav,0,sizeof(wav));
-		wav->dwFrequency=fq;
-		wav->dwChannels=channel;
-		wav->swbyterate=fq*channel*2;
 		
-		if(MMSYSERR_NOERROR!= waveOutOpen(&myDevice,WAVE_MAPPER,
+		wav.wFormatTag=WAVE_FORMAT_PCM;
+		wav.nSamplesPerSec=fq;
+		wav.nChannels=channel;
+		wav.nBlockAlign=2*channel;
+		wav.nAvgBytesPerSec=2*channel*fq;		
+		wav.wBitsPerSample=16;
+		
+		myError=waveOutOpen(&myDevice,WAVE_MAPPER,
   
   				&wav,	//L pwfx,      
-  				NULL, //DWORD_PTR      dwCallback,
+  				(DWORD_PTR)waveOutProc , //0, //DWORD_PTR      dwCallback,
   				0,	//DWORD_PTR      dwCallbackInstance,
-  				WAVE_MAPPED //DWORD          fdwOpen    
-			)
+  				CALLBACK_FUNCTION //CALLBACK_NULL //WAVE_MAPPED //DWORD          fdwOpen    
+			);
+		if(MMSYSERR_NOERROR!=myError )
 			{
 				printf("WavoutOpen failed\n");
+			 	handleMM(myError);
 				return 0;
 			}
-		
+    WAVEHDR *hdr;
+			
+	for(uint32_t i=0;i<NB_BUCKET;i++)
+	{
+		 buckets[i].fre=1;
+		 hdr=&(buckets[i].hdr);
+		 waveOutPrepareHeader(myDevice,hdr,sizeof(*hdr));	
+         hdr->lpData=(CHAR *)new uint8_t [BUCKET_SIZE];
+         hdr->dwBufferLength=0;
+	}
+
 	
     return 1;
 }
@@ -124,26 +179,86 @@ PWAVEFORMATEX wav;
 //
 //
 //_______________________________________________
-uint8_t sdlAudioDevice::play(uint32_t nb,uint8_t * ptr)
+uint8_t win32AudioDevice::play(uint32_t nb,uint8_t * ptr)
  {
-	WAVEHDR hdr;
-	
-	memset(&hdr,0,sizeof(hdr));
-	hdr.lpData=ptr;
-	hdr.dwBufferLength=nb;
-	
-	if( MMSYSERR_NOERROR!=waveOutWrite(
+ uint32_t av=0;
+	WAVEHDR *hdr;
+
+   assert(nb<BUCKET_SIZE);
+   // First do clean up
+#if 0   
+      for(uint32_t i=0;i<NB_BUCKET;i++)
+      {
+            if(!buckets[i].fre)
+            {
+                        if(buckets[i].hdr.dwFlags & WHDR_DONE)
+                        {
+                                                buckets[i].fre=1;
+                                                buckets[i].hdr.dwFlags=0; 
+                                                printf("Freeing buffer %d  \n",i);   
+                                                av++;                                            
+                        }            
+            }
+            else
+                        av++;
+      }
+      printf("Free buffer:%d %d\n",av,NB_BUCKET);
+#endif      
+      // look up a free buffer
+      
+   for(uint32_t i=0;i<NB_BUCKET;i++)
+   {
+      if(buckets[i].fre)
+      {
+            memcpy(buckets[i].hdr.lpData,ptr,nb);
+            buckets[i].hdr.dwBufferLength=nb;
+            buckets[i].fre=0;
+            hdr=&(buckets[i].hdr);
+            myError=waveOutWrite(
   					myDevice, //HWAVEOUT hwo,  
-  					&hdr, //LPWAVEHDR pwh, 
-  					sizeof(hdr) //UINT cbwh      
-			))
+  					hdr, //LPWAVEHDR pwh, 
+  					sizeof(*hdr) //UINT cbwh      
+			  					);
+			if( MMSYSERR_NOERROR!=myError)
 			{
-			
-				printf("win32Audio write failed\n");
-				return 0;
+			    handleMM(myError);
+			    return 0;
 			}
-			
-	return 1;
+			return 1;
+      }
+      
+   
+   }
+    printf("All buckets full !\n");
+	return 0;
+}
+void CALLBACK waveOutProc(
+  HWAVEOUT hwo,      
+  UINT uMsg,         
+  DWORD_PTR dwInstance,  
+  DWORD dwParam1,    
+  DWORD dwParam2     
+)
+{
+    if(uMsg==WOM_DONE)
+    {
+
+        audioBucket *bucket;
+                        bucket=(audioBucket *)(dwParam1-sizeof(int));
+                        bucket->fre=1;
+    }
+
+}
+void 	handleMM(MMRESULT  err)
+{
+ #define ERMM(x) if(err==x) printf(#x"\n");
+    ERMM(MMSYSERR_ALLOCATED);
+    ERMM(MMSYSERR_BADDEVICEID);
+    ERMM(MMSYSERR_NODRIVER);
+    ERMM(WAVERR_BADFORMAT);
+    ERMM(WAVERR_SYNC);
+    
+
 }
 #else
 void dummy_as_funcZ( void);
