09bfd079982e228b2e09a4c5a4299b70d50685ea
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-08-15 09:23:53 +0000

    Better ui by JM

--- trunk/avidemux/avidemux/ADM_dialog/DIA_muxer.cpp
+++ trunk/avidemux/avidemux/ADM_dialog/DIA_muxer.cpp
@@ -42,8 +42,8 @@ uint8_t  DIA_setUserMuxParam( int *mode, int *param, int *muxsize)
                 switch(*mode)
                 	{
                    	case MUX_REGULAR:
-					RADIO_SET(radioEvery,1);
-
+					RADIO_SET(radioN,1);
+					ADJ_SET(spinFrame,1);
 		                    	break;
                    	case MUX_N_FRAMES:
 					RADIO_SET(radioN,1);
@@ -62,18 +62,18 @@ uint8_t  DIA_setUserMuxParam( int *mode, int *param, int *muxsize)
 		{
 				int r; // ,value=0;
 				ret=1;
-				r=RADIO_GET(radioEvery)+(2*RADIO_GET(radioN))+(4*RADIO_GET(radioBytes));
+				r=(RADIO_GET(radioN))+(2*RADIO_GET(radioBytes));
 				printf("\n r: %d \n",r);
 				switch(r)
 				{
-					case 1: // every
-							*mode=MUX_REGULAR;
-							break;
-					case 2: // every N
-							*mode=MUX_N_FRAMES;
+					case 1: // every or N
 							*param=ADJ_GET(spinFrame);
+							if(*param==1)
+								*mode=MUX_REGULAR;
+							else
+								*mode=MUX_N_FRAMES;
 							break;
-					case 4: // byte
+					case 2: // byte
 							*mode=MUX_N_BYTES;
 							*param=ADJ_GET(spinSize);
 							break;
@@ -88,144 +88,142 @@ uint8_t  DIA_setUserMuxParam( int *mode, int *param, int *muxsize)
 	 return ret;
 }
 
-
 GtkWidget	*create_dialog1 (void)
 {
-  GtkWidget *dialog1;
+  GtkWidget *dialog;
   GtkWidget *dialog_vbox1;
-  GtkWidget *vbox1;
-  GtkWidget *label1;
   GtkWidget *table1;
-  GtkWidget *radioEvery;
-  GSList *radioEvery_group = NULL;
   GtkWidget *radioN;
+  GSList *radioN_group = NULL;
   GtkWidget *radioBytes;
-  GtkWidget *hseparator1;
   GtkObject *spinFrame_adj;
   GtkWidget *spinFrame;
   GtkObject *spinSize_adj;
   GtkWidget *spinSize;
-  GtkWidget *vbox2;
+  GtkWidget *label1;
   GtkWidget *label2;
+  GtkWidget *label3;
   GtkObject *spinbutton3_adj;
   GtkWidget *spinbutton3;
+  GtkWidget *label4;
   GtkWidget *dialog_action_area1;
   GtkWidget *cancelbutton1;
   GtkWidget *okbutton1;
 
-  dialog1 = gtk_dialog_new ();
-  gtk_window_set_title (GTK_WINDOW (dialog1), _("Avi Muxer Options"));
+  dialog = gtk_dialog_new ();
+  gtk_window_set_title (GTK_WINDOW (dialog), _("AVI muxer options"));
+  gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
 
-  dialog_vbox1 = GTK_DIALOG (dialog1)->vbox;
+  dialog_vbox1 = GTK_DIALOG (dialog)->vbox;
   gtk_widget_show (dialog_vbox1);
 
-  vbox1 = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox1);
-  gtk_box_pack_start (GTK_BOX (dialog_vbox1), vbox1, TRUE, TRUE, 0);
-
-  label1 = gtk_label_new (_("Mux audio every"));
-  gtk_widget_show (label1);
-  gtk_box_pack_start (GTK_BOX (vbox1), label1, FALSE, FALSE, 0);
-  gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
-
-  table1 = gtk_table_new (3, 2, FALSE);
+  table1 = gtk_table_new (3, 3, FALSE);
   gtk_widget_show (table1);
-  gtk_box_pack_start (GTK_BOX (vbox1), table1, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (dialog_vbox1), table1, TRUE, TRUE, 0);
+  gtk_container_set_border_width (GTK_CONTAINER (table1), 5);
+  gtk_table_set_row_spacings (GTK_TABLE (table1), 5);
+  gtk_table_set_col_spacings (GTK_TABLE (table1), 5);
 
-  radioEvery = gtk_radio_button_new_with_mnemonic (NULL, _("Every Video frame"));
-  gtk_widget_show (radioEvery);
-  gtk_table_attach (GTK_TABLE (table1), radioEvery, 0, 1, 0, 1,
-                    (GtkAttachOptions) (GTK_FILL),
-                    (GtkAttachOptions) (0), 0, 0);
-  gtk_radio_button_set_group (GTK_RADIO_BUTTON (radioEvery), radioEvery_group);
-  radioEvery_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (radioEvery));
-
-  radioN = gtk_radio_button_new_with_mnemonic (NULL, _("Every N video frames"));
+  radioN = gtk_radio_button_new_with_mnemonic (NULL, _("Mux audio every"));
   gtk_widget_show (radioN);
-  gtk_table_attach (GTK_TABLE (table1), radioN, 0, 1, 1, 2,
+  gtk_table_attach (GTK_TABLE (table1), radioN, 0, 1, 0, 1,
                     (GtkAttachOptions) (GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_radio_button_set_group (GTK_RADIO_BUTTON (radioN), radioEvery_group);
-  radioEvery_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (radioN));
+  gtk_radio_button_set_group (GTK_RADIO_BUTTON (radioN), radioN_group);
+  radioN_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (radioN));
 
-  radioBytes = gtk_radio_button_new_with_mnemonic (NULL, _("Split in packet of size"));
+  radioBytes = gtk_radio_button_new_with_mnemonic (NULL, _("Split in packets of"));
   gtk_widget_show (radioBytes);
-  gtk_table_attach (GTK_TABLE (table1), radioBytes, 0, 1, 2, 3,
+  gtk_table_attach (GTK_TABLE (table1), radioBytes, 0, 1, 1, 2,
                     (GtkAttachOptions) (GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_radio_button_set_group (GTK_RADIO_BUTTON (radioBytes), radioEvery_group);
-  radioEvery_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (radioBytes));
-
-  hseparator1 = gtk_hseparator_new ();
-  gtk_widget_show (hseparator1);
-  gtk_table_attach (GTK_TABLE (table1), hseparator1, 1, 2, 0, 1,
-                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
-                    (GtkAttachOptions) (GTK_FILL), 0, 0);
+  gtk_radio_button_set_group (GTK_RADIO_BUTTON (radioBytes), radioN_group);
+  radioN_group = gtk_radio_button_get_group (GTK_RADIO_BUTTON (radioBytes));
 
   spinFrame_adj = gtk_adjustment_new (1, 0, 100, 1, 10, 10);
   spinFrame = gtk_spin_button_new (GTK_ADJUSTMENT (spinFrame_adj), 1, 0);
   gtk_widget_show (spinFrame);
-  gtk_table_attach (GTK_TABLE (table1), spinFrame, 1, 2, 1, 2,
+  gtk_table_attach (GTK_TABLE (table1), spinFrame, 1, 2, 0, 1,
                     (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinFrame), TRUE);
 
   spinSize_adj = gtk_adjustment_new (2048, 0, 100000, 1, 10, 10);
   spinSize = gtk_spin_button_new (GTK_ADJUSTMENT (spinSize_adj), 1, 0);
   gtk_widget_show (spinSize);
-  gtk_table_attach (GTK_TABLE (table1), spinSize, 1, 2, 2, 3,
+  gtk_table_attach (GTK_TABLE (table1), spinSize, 1, 2, 1, 2,
                     (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinSize), TRUE);
 
-  vbox2 = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox2);
-  gtk_box_pack_start (GTK_BOX (vbox1), vbox2, TRUE, TRUE, 0);
+  label1 = gtk_label_new (_("video frame(s)"));
+  gtk_widget_show (label1);
+  gtk_table_attach (GTK_TABLE (table1), label1, 2, 3, 0, 1,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label1), 0, 0.5);
 
-  label2 = gtk_label_new (_("Autosplit file every N megabytes"));
+  label2 = gtk_label_new (_("bytes"));
   gtk_widget_show (label2);
-  gtk_box_pack_start (GTK_BOX (vbox2), label2, FALSE, FALSE, 0);
+  gtk_table_attach (GTK_TABLE (table1), label2, 2, 3, 1, 2,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
   gtk_label_set_justify (GTK_LABEL (label2), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label2), 0, 0.5);
 
-  spinbutton3_adj = gtk_adjustment_new (10, 10, 4095, 1, 10, 10);
+  label3 = gtk_label_new (_("Autosplit file every"));
+  gtk_widget_show (label3);
+  gtk_table_attach (GTK_TABLE (table1), label3, 0, 1, 2, 3,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label3), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label3), 0, 0.5);
+
+  spinbutton3_adj = gtk_adjustment_new (4090, 10, 4095, 1, 10, 10);
   spinbutton3 = gtk_spin_button_new (GTK_ADJUSTMENT (spinbutton3_adj), 1, 0);
   gtk_widget_show (spinbutton3);
-  gtk_box_pack_start (GTK_BOX (vbox2), spinbutton3, FALSE, FALSE, 0);
-  gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinbutton3), TRUE);
+  gtk_table_attach (GTK_TABLE (table1), spinbutton3, 1, 2, 2, 3,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
 
-  dialog_action_area1 = GTK_DIALOG (dialog1)->action_area;
+  label4 = gtk_label_new (_("megabytes"));
+  gtk_widget_show (label4);
+  gtk_table_attach (GTK_TABLE (table1), label4, 2, 3, 2, 3,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label4), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label4), 0, 0.5);
+
+  dialog_action_area1 = GTK_DIALOG (dialog)->action_area;
   gtk_widget_show (dialog_action_area1);
   gtk_button_box_set_layout (GTK_BUTTON_BOX (dialog_action_area1), GTK_BUTTONBOX_END);
 
   cancelbutton1 = gtk_button_new_from_stock ("gtk-cancel");
   gtk_widget_show (cancelbutton1);
-  gtk_dialog_add_action_widget (GTK_DIALOG (dialog1), cancelbutton1, GTK_RESPONSE_CANCEL);
+  gtk_dialog_add_action_widget (GTK_DIALOG (dialog), cancelbutton1, GTK_RESPONSE_CANCEL);
   GTK_WIDGET_SET_FLAGS (cancelbutton1, GTK_CAN_DEFAULT);
 
   okbutton1 = gtk_button_new_from_stock ("gtk-ok");
   gtk_widget_show (okbutton1);
-  gtk_dialog_add_action_widget (GTK_DIALOG (dialog1), okbutton1, GTK_RESPONSE_OK);
+  gtk_dialog_add_action_widget (GTK_DIALOG (dialog), okbutton1, GTK_RESPONSE_OK);
   GTK_WIDGET_SET_FLAGS (okbutton1, GTK_CAN_DEFAULT);
 
   /* Store pointers to all widgets, for use by lookup_widget(). */
-  GLADE_HOOKUP_OBJECT_NO_REF (dialog1, dialog1, "dialog1");
-  GLADE_HOOKUP_OBJECT_NO_REF (dialog1, dialog_vbox1, "dialog_vbox1");
-  GLADE_HOOKUP_OBJECT (dialog1, vbox1, "vbox1");
-  GLADE_HOOKUP_OBJECT (dialog1, label1, "label1");
-  GLADE_HOOKUP_OBJECT (dialog1, table1, "table1");
-  GLADE_HOOKUP_OBJECT (dialog1, radioEvery, "radioEvery");
-  GLADE_HOOKUP_OBJECT (dialog1, radioN, "radioN");
-  GLADE_HOOKUP_OBJECT (dialog1, radioBytes, "radioBytes");
-  GLADE_HOOKUP_OBJECT (dialog1, hseparator1, "hseparator1");
-  GLADE_HOOKUP_OBJECT (dialog1, spinFrame, "spinFrame");
-  GLADE_HOOKUP_OBJECT (dialog1, spinSize, "spinSize");
-  GLADE_HOOKUP_OBJECT (dialog1, vbox2, "vbox2");
-  GLADE_HOOKUP_OBJECT (dialog1, label2, "label2");
-  GLADE_HOOKUP_OBJECT (dialog1, spinbutton3, "spinbutton3");
-  GLADE_HOOKUP_OBJECT_NO_REF (dialog1, dialog_action_area1, "dialog_action_area1");
-  GLADE_HOOKUP_OBJECT (dialog1, cancelbutton1, "cancelbutton1");
-  GLADE_HOOKUP_OBJECT (dialog1, okbutton1, "okbutton1");
+  GLADE_HOOKUP_OBJECT_NO_REF (dialog, dialog, "dialog");
+  GLADE_HOOKUP_OBJECT_NO_REF (dialog, dialog_vbox1, "dialog_vbox1");
+  GLADE_HOOKUP_OBJECT (dialog, table1, "table1");
+  GLADE_HOOKUP_OBJECT (dialog, radioN, "radioN");
+  GLADE_HOOKUP_OBJECT (dialog, radioBytes, "radioBytes");
+  GLADE_HOOKUP_OBJECT (dialog, spinFrame, "spinFrame");
+  GLADE_HOOKUP_OBJECT (dialog, spinSize, "spinSize");
+  GLADE_HOOKUP_OBJECT (dialog, label1, "label1");
+  GLADE_HOOKUP_OBJECT (dialog, label2, "label2");
+  GLADE_HOOKUP_OBJECT (dialog, label3, "label3");
+  GLADE_HOOKUP_OBJECT (dialog, spinbutton3, "spinbutton3");
+  GLADE_HOOKUP_OBJECT (dialog, label4, "label4");
+  GLADE_HOOKUP_OBJECT_NO_REF (dialog, dialog_action_area1, "dialog_action_area1");
+  GLADE_HOOKUP_OBJECT (dialog, cancelbutton1, "cancelbutton1");
+  GLADE_HOOKUP_OBJECT (dialog, okbutton1, "okbutton1");
 
-  return dialog1;
+  return dialog;
 }
 
