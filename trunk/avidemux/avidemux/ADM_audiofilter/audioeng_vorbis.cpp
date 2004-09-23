bc4e068be7960c83549de96c030a69b178e7bf95
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-09-23 18:09:16 +0000

    try to get as much as possible

--- trunk/avidemux/avidemux/ADM_audiofilter/audioeng_vorbis.cpp
+++ trunk/avidemux/avidemux/ADM_audiofilter/audioeng_vorbis.cpp
@@ -204,9 +204,16 @@ while(count--)
 
 	asked=FRAME*2*_wavheader->channels;
 	rd=_instream->read(asked,(uint8_t *)buf);
+	if(!rd)
+	{
+		rd=asked;
+		memset(buf,0,rd);
+		printf("Vorbis : No audio : padding\n");
+	
+	}
+
 	nbSample=rd/(2*_wavheader->channels);
 	
-	if(!nbSample) return 0;
 	total+=nbSample;
 	//aprintf("Nb Sample:%lu\n",nbSample);
 	// Now we got out samples, encode them
