06715dccdd6315aa0ee7575652e17ac1aa3816d9
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-10-09 17:58:20 +0000

    really simple bitrate calculator

---
+++ trunk/avidemux/avidemux/ADM_dialog/DIA_calculator.cpp
@@ -0,0 +1,518 @@
+/***************************************************************************
+                          Simple bitrate calculator
+                             -------------------
+    begin                : Wed Dec 18 2002
+    copyright            : (C) 2004 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
+#include <gtk/gtk.h>
+
+#include "fourcc.h"
+#include "avio.hxx"
+
+
+#include "avi_vars.h"
+#include "ADM_toolkit/toolkit_gtk.h"
+#include "ADM_toolkit/toolkit_gtk_include.h"
+#include "ADM_toolkit/toolkit.hxx"
+#include "ADM_toolkit/filesel.h"
+#include "ADM_assert.h"
+
+
+
+#include "ADM_editor/ADM_edit.hxx"
+#include "ADM_video/ADM_genvideo.hxx"
+
+
+#include "ADM_audiofilter/audioprocess.hxx"
+#include "ADM_audiofilter/audioeng_buildfilters.h"
+#include "ADM_audiofilter/audioeng_film2pal.h"
+
+
+void DIA_Calculator(void );
+static GtkWidget	*create_Calculator (void);  
+static GtkWidget *dialog;  
+static void prepare( void );
+static void update( void );
+
+static uint32_t videoDuration;
+static uint32_t track1, track2;
+
+//************************************
+void DIA_Calculator(void )
+{
+	if(!avifileinfo) return ;
+	
+	dialog=create_Calculator();
+	prepare();
+	update();
+	while(1)
+	{
+		if(GTK_RESPONSE_APPLY==gtk_dialog_run(GTK_DIALOG(dialog)))
+		{
+			update();
+		}
+	else
+		{
+			break;
+		}
+	}
+	gtk_widget_destroy(dialog);
+
+}
+//******************************************
+// Compute the value that will not change
+// (duration)
+// and init the entries for current bitrate
+//******************************************
+void prepare( void )
+{
+
+	float duration=0;
+	aviInfo info;
+	char string[200];
+	 uint16_t mm,hh,ss,ms;
+	 uint32_t frame;
+	 AVDMProcessAudioStream *stream;
+	 
+	 if(frameStart<frameEnd) frame=frameEnd-frameStart;
+	 else			 frame=frameStart-frameEnd;
+ 
+ 	
+	
+	duration=(float)video_body->getTime (frame);
+	duration=duration/1000.;
+	
+	if(duration<0) duration=-duration;
+	
+	video_body->getVideoInfo(&info);
+	
+	videoDuration=(uint32_t)ceil(duration);
+	frame2time(frame,info.fps1000, &hh, &mm, &ss, &ms);
+	// now we can set it
+	sprintf(string,"%02d:%02d:%02d",hh,mm,ss);
+	gtk_label_set_text(GTK_LABEL(WID(labelDuration)),string);
+	
+	printf("Video duration :%lu\n",videoDuration);
+
+	// Now get audio info
+	track1=0;
+	if(audioProcessMode && currentaudiostream)
+	{
+		stream=buildAudioFilter(currentaudiostream,
+				0, 0xffffffff);
+	
+		if(stream)
+		{
+			track1=(stream->getInfo()->byterate*8)/1000;
+		}
+		deleteAudioFilter();
+	}else
+	{
+		if(currentaudiostream) track1=(currentaudiostream->getInfo()->byterate*8)/1000;
+	}
+	
+	track2=0;
+	gtk_write_entry(WID(entry3), track1);
+	gtk_write_entry(WID(entry4), track2);
+	
+	printf("Track1 bitrate :%lu\n",track1);
+	printf("Track2 bitrate :%lu\n",track2);
+
+	
+	
+}
+
+//*************************************
+void update( void )
+{
+uint32_t audioSize;
+uint32_t totalSize;
+char string[200];
+	track1=gtk_read_entry(WID(entry3));
+	track2=gtk_read_entry(WID(entry4));
+	gtk_write_entry(WID(entry3), track1);
+	gtk_write_entry(WID(entry4), track2);
+	
+	// kb->Byte
+	audioSize=(track1+track2)*1000;
+	audioSize/=8;
+	
+	audioSize*=videoDuration;	
+	audioSize>>=20;
+	sprintf(string,"%lu",audioSize);
+	gtk_label_set_text(GTK_LABEL(WID(labelAudio)),string);
+	
+	// Compute total size (for Avi)
+	uint32_t s74,s80,dvd;
+	
+	// For avi/ogm 
+	if(getRangeInMenu(WID(optionmenu2))==2)
+	{ // Mpeg
+		s74=730;
+		s80=790;
+		dvd=4300;
+	
+	}
+	else
+	{//AVI or OGM
+		s74=690;
+		s80=730;
+		dvd=4300;
+	}
+	
+	int j=getRangeInMenu(WID(optionmenu1));
+	switch(j)
+	{
+		case 0: totalSize=1*s74;break;
+		case 1: totalSize=2*s74;break;
+		case 2: totalSize=1*s80;break;
+		case 3: totalSize=2*s80;break;
+		case 4: totalSize=dvd;break;
+		default:
+			ADM_assert(0);
+	}
+	sprintf(string,"%lu",totalSize);
+	gtk_label_set_text(GTK_LABEL(WID(labelTotal)),string);
+	
+	// and compute
+	//
+	uint32_t videoSize;
+	
+	if(audioSize>=totalSize) sprintf(string,"** NO ROOM LEFT**");
+	else
+		{
+			videoSize=totalSize-audioSize;
+			sprintf(string,"%lu",videoSize);
+		}
+	gtk_label_set_text(GTK_LABEL(WID(labelVideo)),string);
+	
+	
+}
+//*************************************
+
+GtkWidget*
+create_Calculator (void)
+{
+  GtkWidget *Calculator;
+  GtkWidget *dialog_vbox1;
+  GtkWidget *vbox1;
+  GtkWidget *frame1;
+  GtkWidget *hbox1;
+  GtkWidget *hbox2;
+  GtkWidget *label2;
+  GtkWidget *optionmenu1;
+  GtkWidget *menu1;
+  GtkWidget *_1x74_cd1;
+  GtkWidget *_2x74_cd1;
+  GtkWidget *_1x80_cd1;
+  GtkWidget *_2x80_cd1;
+  GtkWidget *dvd1;
+  GtkWidget *hbox3;
+  GtkWidget *label3;
+  GtkWidget *optionmenu2;
+  GtkWidget *menu2;
+  GtkWidget *avi1;
+  GtkWidget *ogm1;
+  GtkWidget *mpeg1;
+  GtkWidget *label1;
+  GtkWidget *frame2;
+  GtkWidget *hbox4;
+  GtkWidget *label6;
+  GtkWidget *entry3;
+  GtkWidget *label7;
+  GtkWidget *entry4;
+  GtkWidget *label4;
+  GtkWidget *frame3;
+  GtkWidget *table1;
+  GtkWidget *label11;
+  GtkWidget *label10;
+  GtkWidget *label9;
+  GtkWidget *labelDOO;
+  GtkWidget *labelDuration;
+  GtkWidget *labelAudio;
+  GtkWidget *labelTotal;
+  GtkWidget *labelVideo;
+  GtkWidget *label8;
+  GtkWidget *dialog_action_area1;
+  GtkWidget *doit;
+  GtkWidget *button1;
+
+  Calculator = gtk_dialog_new ();
+  gtk_window_set_title (GTK_WINDOW (Calculator), _("Calculator"));
+
+  dialog_vbox1 = GTK_DIALOG (Calculator)->vbox;
+  gtk_widget_show (dialog_vbox1);
+
+  vbox1 = gtk_vbox_new (FALSE, 0);
+  gtk_widget_show (vbox1);
+  gtk_box_pack_start (GTK_BOX (dialog_vbox1), vbox1, TRUE, TRUE, 0);
+
+  frame1 = gtk_frame_new (NULL);
+  gtk_widget_show (frame1);
+  gtk_box_pack_start (GTK_BOX (vbox1), frame1, TRUE, TRUE, 0);
+
+  hbox1 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox1);
+  gtk_container_add (GTK_CONTAINER (frame1), hbox1);
+
+  hbox2 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox2);
+  gtk_box_pack_start (GTK_BOX (hbox1), hbox2, TRUE, TRUE, 0);
+
+  label2 = gtk_label_new (_("Target Medium :"));
+  gtk_widget_show (label2);
+  gtk_box_pack_start (GTK_BOX (hbox2), label2, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (label2), GTK_JUSTIFY_LEFT);
+
+  optionmenu1 = gtk_option_menu_new ();
+  gtk_widget_show (optionmenu1);
+  gtk_box_pack_start (GTK_BOX (hbox2), optionmenu1, FALSE, FALSE, 0);
+
+  menu1 = gtk_menu_new ();
+
+  _1x74_cd1 = gtk_menu_item_new_with_mnemonic (_("1x74 Cd"));
+  gtk_widget_show (_1x74_cd1);
+  gtk_container_add (GTK_CONTAINER (menu1), _1x74_cd1);
+
+  _2x74_cd1 = gtk_menu_item_new_with_mnemonic (_("2x74 Cd"));
+  gtk_widget_show (_2x74_cd1);
+  gtk_container_add (GTK_CONTAINER (menu1), _2x74_cd1);
+
+  _1x80_cd1 = gtk_menu_item_new_with_mnemonic (_("1x80 Cd"));
+  gtk_widget_show (_1x80_cd1);
+  gtk_container_add (GTK_CONTAINER (menu1), _1x80_cd1);
+
+  _2x80_cd1 = gtk_menu_item_new_with_mnemonic (_("2x80 Cd"));
+  gtk_widget_show (_2x80_cd1);
+  gtk_container_add (GTK_CONTAINER (menu1), _2x80_cd1);
+
+  dvd1 = gtk_menu_item_new_with_mnemonic (_("DVD5"));
+  gtk_widget_show (dvd1);
+  gtk_container_add (GTK_CONTAINER (menu1), dvd1);
+
+  gtk_option_menu_set_menu (GTK_OPTION_MENU (optionmenu1), menu1);
+
+  hbox3 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox3);
+  gtk_box_pack_start (GTK_BOX (hbox1), hbox3, TRUE, TRUE, 0);
+
+  label3 = gtk_label_new (_("Target Format :"));
+  gtk_widget_show (label3);
+  gtk_box_pack_start (GTK_BOX (hbox3), label3, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (label3), GTK_JUSTIFY_LEFT);
+
+  optionmenu2 = gtk_option_menu_new ();
+  gtk_widget_show (optionmenu2);
+  gtk_box_pack_start (GTK_BOX (hbox3), optionmenu2, FALSE, FALSE, 0);
+
+  menu2 = gtk_menu_new ();
+
+  avi1 = gtk_menu_item_new_with_mnemonic (_("Avi"));
+  gtk_widget_show (avi1);
+  gtk_container_add (GTK_CONTAINER (menu2), avi1);
+
+  ogm1 = gtk_menu_item_new_with_mnemonic (_("OGM"));
+  gtk_widget_show (ogm1);
+  gtk_container_add (GTK_CONTAINER (menu2), ogm1);
+
+  mpeg1 = gtk_menu_item_new_with_mnemonic (_("Mpeg"));
+  gtk_widget_show (mpeg1);
+  gtk_container_add (GTK_CONTAINER (menu2), mpeg1);
+
+  gtk_option_menu_set_menu (GTK_OPTION_MENU (optionmenu2), menu2);
+
+  label1 = gtk_label_new (_("<b>Target</b>"));
+  gtk_widget_show (label1);
+  gtk_frame_set_label_widget (GTK_FRAME (frame1), label1);
+  gtk_label_set_use_markup (GTK_LABEL (label1), TRUE);
+  gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
+
+  frame2 = gtk_frame_new (NULL);
+  gtk_widget_show (frame2);
+  gtk_box_pack_start (GTK_BOX (vbox1), frame2, TRUE, TRUE, 0);
+
+  hbox4 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox4);
+  gtk_container_add (GTK_CONTAINER (frame2), hbox4);
+
+  label6 = gtk_label_new (_("Track 1 :"));
+  gtk_widget_show (label6);
+  gtk_box_pack_start (GTK_BOX (hbox4), label6, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (label6), GTK_JUSTIFY_LEFT);
+
+  entry3 = gtk_entry_new ();
+  gtk_widget_show (entry3);
+  gtk_box_pack_start (GTK_BOX (hbox4), entry3, TRUE, TRUE, 0);
+
+  label7 = gtk_label_new (_("Track 2 : "));
+  gtk_widget_show (label7);
+  gtk_box_pack_start (GTK_BOX (hbox4), label7, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (label7), GTK_JUSTIFY_LEFT);
+
+  entry4 = gtk_entry_new ();
+  gtk_widget_show (entry4);
+  gtk_box_pack_start (GTK_BOX (hbox4), entry4, TRUE, TRUE, 0);
+
+  label4 = gtk_label_new (_("<b>Audio</b>"));
+  gtk_widget_show (label4);
+  gtk_frame_set_label_widget (GTK_FRAME (frame2), label4);
+  gtk_label_set_use_markup (GTK_LABEL (label4), TRUE);
+  gtk_label_set_justify (GTK_LABEL (label4), GTK_JUSTIFY_LEFT);
+
+  frame3 = gtk_frame_new (NULL);
+  gtk_widget_show (frame3);
+  gtk_box_pack_start (GTK_BOX (vbox1), frame3, TRUE, TRUE, 0);
+
+  table1 = gtk_table_new (4, 2, FALSE);
+  gtk_widget_show (table1);
+  gtk_container_add (GTK_CONTAINER (frame3), table1);
+
+  label11 = gtk_label_new (_("Video Size (MBytes)"));
+  gtk_widget_show (label11);
+  gtk_table_attach (GTK_TABLE (table1), label11, 0, 1, 3, 4,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label11), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label11), 0, 0.5);
+
+  label10 = gtk_label_new (_("Total Size (MBytes)"));
+  gtk_widget_show (label10);
+  gtk_table_attach (GTK_TABLE (table1), label10, 0, 1, 2, 3,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label10), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label10), 0, 0.5);
+
+  label9 = gtk_label_new (_("Audio size (MBytes)"));
+  gtk_widget_show (label9);
+  gtk_table_attach (GTK_TABLE (table1), label9, 0, 1, 1, 2,
+                    (GtkAttachOptions) (GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (label9), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (label9), 0, 0.5);
+
+  labelDOO = gtk_label_new (_("Duration "));
+  gtk_widget_show (labelDOO);
+  gtk_table_attach (GTK_TABLE (table1), labelDOO, 0, 1, 0, 1,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (labelDOO), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (labelDOO), 0, 0.5);
+
+  labelDuration = gtk_label_new (_("00:00:00"));
+  gtk_widget_show (labelDuration);
+  gtk_table_attach (GTK_TABLE (table1), labelDuration, 1, 2, 0, 1,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (labelDuration), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (labelDuration), 0, 0.5);
+
+  labelAudio = gtk_label_new (_("0"));
+  gtk_widget_show (labelAudio);
+  gtk_table_attach (GTK_TABLE (table1), labelAudio, 1, 2, 1, 2,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (labelAudio), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (labelAudio), 0, 0.5);
+
+  labelTotal = gtk_label_new (_("0"));
+  gtk_widget_show (labelTotal);
+  gtk_table_attach (GTK_TABLE (table1), labelTotal, 1, 2, 2, 3,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (labelTotal), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (labelTotal), 0, 0.5);
+
+  labelVideo = gtk_label_new (_("0"));
+  gtk_widget_show (labelVideo);
+  gtk_table_attach (GTK_TABLE (table1), labelVideo, 1, 2, 3, 4,
+                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0), 0, 0);
+  gtk_label_set_justify (GTK_LABEL (labelVideo), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (labelVideo), 0, 0.5);
+
+  label8 = gtk_label_new (_("<b>Result</b>"));
+  gtk_widget_show (label8);
+  gtk_frame_set_label_widget (GTK_FRAME (frame3), label8);
+  gtk_label_set_use_markup (GTK_LABEL (label8), TRUE);
+  gtk_label_set_justify (GTK_LABEL (label8), GTK_JUSTIFY_LEFT);
+
+  dialog_action_area1 = GTK_DIALOG (Calculator)->action_area;
+  gtk_widget_show (dialog_action_area1);
+  gtk_button_box_set_layout (GTK_BUTTON_BOX (dialog_action_area1), GTK_BUTTONBOX_END);
+
+  doit = gtk_button_new_from_stock ("gtk-apply");
+  gtk_widget_show (doit);
+  gtk_dialog_add_action_widget (GTK_DIALOG (Calculator), doit, GTK_RESPONSE_APPLY);
+  GTK_WIDGET_SET_FLAGS (doit, GTK_CAN_DEFAULT);
+
+  button1 = gtk_button_new_from_stock ("gtk-close");
+  gtk_widget_show (button1);
+  gtk_dialog_add_action_widget (GTK_DIALOG (Calculator), button1, GTK_RESPONSE_CLOSE);
+  GTK_WIDGET_SET_FLAGS (button1, GTK_CAN_DEFAULT);
+
+  /* Store pointers to all widgets, for use by lookup_widget(). */
+  GLADE_HOOKUP_OBJECT_NO_REF (Calculator, Calculator, "Calculator");
+  GLADE_HOOKUP_OBJECT_NO_REF (Calculator, dialog_vbox1, "dialog_vbox1");
+  GLADE_HOOKUP_OBJECT (Calculator, vbox1, "vbox1");
+  GLADE_HOOKUP_OBJECT (Calculator, frame1, "frame1");
+  GLADE_HOOKUP_OBJECT (Calculator, hbox1, "hbox1");
+  GLADE_HOOKUP_OBJECT (Calculator, hbox2, "hbox2");
+  GLADE_HOOKUP_OBJECT (Calculator, label2, "label2");
+  GLADE_HOOKUP_OBJECT (Calculator, optionmenu1, "optionmenu1");
+  GLADE_HOOKUP_OBJECT (Calculator, menu1, "menu1");
+  GLADE_HOOKUP_OBJECT (Calculator, _1x74_cd1, "_1x74_cd1");
+  GLADE_HOOKUP_OBJECT (Calculator, _2x74_cd1, "_2x74_cd1");
+  GLADE_HOOKUP_OBJECT (Calculator, _1x80_cd1, "_1x80_cd1");
+  GLADE_HOOKUP_OBJECT (Calculator, _2x80_cd1, "_2x80_cd1");
+  GLADE_HOOKUP_OBJECT (Calculator, dvd1, "dvd1");
+  GLADE_HOOKUP_OBJECT (Calculator, hbox3, "hbox3");
+  GLADE_HOOKUP_OBJECT (Calculator, label3, "label3");
+  GLADE_HOOKUP_OBJECT (Calculator, optionmenu2, "optionmenu2");
+  GLADE_HOOKUP_OBJECT (Calculator, menu2, "menu2");
+  GLADE_HOOKUP_OBJECT (Calculator, avi1, "avi1");
+  GLADE_HOOKUP_OBJECT (Calculator, ogm1, "ogm1");
+  GLADE_HOOKUP_OBJECT (Calculator, mpeg1, "mpeg1");
+  GLADE_HOOKUP_OBJECT (Calculator, label1, "label1");
+  GLADE_HOOKUP_OBJECT (Calculator, frame2, "frame2");
+  GLADE_HOOKUP_OBJECT (Calculator, hbox4, "hbox4");
+  GLADE_HOOKUP_OBJECT (Calculator, label6, "label6");
+  GLADE_HOOKUP_OBJECT (Calculator, entry3, "entry3");
+  GLADE_HOOKUP_OBJECT (Calculator, label7, "label7");
+  GLADE_HOOKUP_OBJECT (Calculator, entry4, "entry4");
+  GLADE_HOOKUP_OBJECT (Calculator, label4, "label4");
+  GLADE_HOOKUP_OBJECT (Calculator, frame3, "frame3");
+  GLADE_HOOKUP_OBJECT (Calculator, table1, "table1");
+  GLADE_HOOKUP_OBJECT (Calculator, label11, "label11");
+  GLADE_HOOKUP_OBJECT (Calculator, label10, "label10");
+  GLADE_HOOKUP_OBJECT (Calculator, label9, "label9");
+  GLADE_HOOKUP_OBJECT (Calculator, labelDOO, "labelDOO");
+  GLADE_HOOKUP_OBJECT (Calculator, labelDuration, "labelDuration");
+  GLADE_HOOKUP_OBJECT (Calculator, labelAudio, "labelAudio");
+  GLADE_HOOKUP_OBJECT (Calculator, labelTotal, "labelTotal");
+  GLADE_HOOKUP_OBJECT (Calculator, labelVideo, "labelVideo");
+  GLADE_HOOKUP_OBJECT (Calculator, label8, "label8");
+  GLADE_HOOKUP_OBJECT_NO_REF (Calculator, dialog_action_area1, "dialog_action_area1");
+  GLADE_HOOKUP_OBJECT (Calculator, doit, "doit");
+  GLADE_HOOKUP_OBJECT (Calculator, button1, "button1");
+
+  return Calculator;
+}
+
--- trunk/avidemux/avidemux/ADM_dialog/DIA_vcodec.cpp
+++ trunk/avidemux/avidemux/ADM_dialog/DIA_vcodec.cpp
@@ -9,62 +9,61 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include "ADM_toolkit/toolkit_gtk_include.h"
+#include "ADM_toolkit/toolkit_gtk.h"
 
-#include <gdk/gdkkeysyms.h>
-#include <gtk/gtk.h>
+#include "ADM_assert.h" 
 
-
-
-#include "ADM_gui2/support.h"
 #include "ADM_library/default.h"
 
 //___________________________________
-//#include "avi_vars.h"
 #include "ADM_editor/ADM_edit.hxx"
 #include "ADM_video/ADM_genvideo.hxx"
 
 #include "ADM_encoder/ADM_vidEncode.hxx"
 #include "ADM_encoder/adm_encoder.h"
-#include "ADM_toolkit/toolkit_gtk.h"
-#include "ADM_assert.h" 
-//___________________________________
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-  g_object_set_data_full (G_OBJECT (component), name, \
-    gtk_widget_ref (widget), (GDestroyNotify) gtk_widget_unref)
 
-#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
-  g_object_set_data (G_OBJECT (component), name, widget)
+
+//___________________________________
 
  static GtkWidget	*create_dialogVideoCodec (void) ;
  static SelectCodecType findCodec( void );
  static GtkWidget *dialog;
  static void okCallback(GtkButton * button, gpointer user_data);
- static SelectCodecType myTab[]=
+ static void callBackCalc( void );
+
+ extern void DIA_Calculator(void);
+ typedef struct myVideoCodec
  {
- 	CodecFF,
-	CodecH263,
-	CodecH263P,
-	CodecHuff,
-	CodecFFV1,
-	CodecSnow,
+ 		SelectCodecType codec;
+		char		*name;
+ }myVideoCodec;
+ static myVideoCodec myTab[]=
+ {
+ 	{CodecFF,"Lavcodec Mpeg4"},
+	{CodecH263,"Lavcodec H263"},
+	{CodecH263P,"Lavcodec H263+"},
+	{CodecHuff,"Huffyuv"},
+	{CodecFFV1,"FFV1"},
+	{CodecSnow,"Snow"},
 #ifdef USE_XVID_4	
- 	CodecXvid4	,
+ 	{CodecXvid4,"Xvid4"},
 #endif	
 #ifdef USE_XX_XVID	
- 	CodecXvid	,
+ 	{CodecXvid,"Xvid"},
 #endif
 #ifdef USE_DIVX
-	CodecDivx	,
+	{CodecDivx,"Divx"},
 #endif
 
-	CodecMjpeg	,
+	{CodecMjpeg,"Mjpeg"},
 #ifdef USE_MJPEG
-	CodecVCD,
-	CodecSVCD,
-	CodecDVD,
+	{CodecVCD,"VCD"},
+	{CodecSVCD,"SVCD"},
+	{CodecDVD,"DVD"},
 #endif
-	CodecXVCD,
-	CodecXSVCD,
+	{CodecXVCD,"XVCD"},
+	{CodecXSVCD,"DVD (lavcodec)"}
 	
  };
 
@@ -80,23 +79,41 @@
 		videoCodecConfigureUI();
 
 }
+
+//___________________________________________________________
+void callBackCalc( void )
+{
+	DIA_Calculator();
+}
+//___________________________________________________________
 uint8_t DIA_videoCodec( SelectCodecType *codec )
 {
 
 	uint8_t ret=0;
 	SelectCodecType old=*codec;
-	
+	GtkWidget *widget[10];
 	dialog=create_dialogVideoCodec();
-	gtk_transient(dialog);	
+	//gtk_transient(dialog);	
+	gtk_register_dialog(dialog);
 
 	
 	// now set the input one
-	for(uint32_t i=0;i<sizeof(myTab)/sizeof(SelectCodecType);i++)
-		if(*codec==myTab[i])
+	for(uint32_t i=0;i<sizeof(myTab)/sizeof(myVideoCodec);i++)
+	{
+		widget[i]=gtk_menu_item_new_with_mnemonic (myTab[i].name);
+  		gtk_widget_show (widget[i]);
+  		gtk_container_add (GTK_CONTAINER (WID(menu1)), widget[i]);
+		if(*codec==myTab[i].codec)
 			{
 				// set
-				gtk_option_menu_set_history(GTK_OPTION_MENU(lookup_widget(dialog,"optionmenu_CodecList")), i);
+				gtk_option_menu_set_history(GTK_OPTION_MENU(WID(optionmenu_CodecList)), i);
 			}
+	}
+	gtk_signal_connect(GTK_OBJECT(WID(buttonConfigure)), "clicked",
+		       GTK_SIGNAL_FUNC(okCallback), (void *) 0);
+	gtk_signal_connect(GTK_OBJECT(WID(buttonCalc)), "clicked",
+		       GTK_SIGNAL_FUNC(callBackCalc), (void *) 0);
+
 	if(gtk_dialog_run(GTK_DIALOG(dialog))==GTK_RESPONSE_OK)
 	{
 		
@@ -109,52 +126,43 @@ uint8_t DIA_videoCodec( SelectCodecType *codec )
 		*codec=old;
 	}
 	gtk_widget_destroy(dialog);
-	
+	gtk_unregister_dialog(dialog);
 	return ret;
 } 
 
 SelectCodecType findCodec( void )
 {
 uint8_t j;
-			j=getRangeInMenu(lookup_widget(dialog,"optionmenu_CodecList"));
+			j=getRangeInMenu(WID(optionmenu_CodecList));
 
-			if(j>=sizeof(myTab)/sizeof(SelectCodecType)) ADM_assert(0);
-			return myTab[j];
+			if(j>=sizeof(myTab)/sizeof(myVideoCodec)) ADM_assert(0);
+			return myTab[j].codec;
 			
 
 }
 
-GtkWidget	*create_dialogVideoCodec (void)
+
+GtkWidget*
+create_dialogVideoCodec (void)
 {
   GtkWidget *dialogVideoCodec;
   GtkWidget *dialog_vbox1;
   GtkWidget *vbox1;
+  GtkWidget *hbox2;
   GtkWidget *optionmenu_CodecList;
   GtkWidget *menu1;
+#if 0
   GtkWidget *ffmpeg_mpeg4;
   GtkWidget *ffmpeg_h263;
   GtkWidget *ffmpeg_h263p;
   GtkWidget *ffmpeg_huffyuv;
-  GtkWidget *ffmpeg_ffv1;
-   GtkWidget *ffmpeg_snow;
-#ifdef USE_XX_XVID
   GtkWidget *xvid;
-#endif
-#ifdef USE_XVID_4
-  GtkWidget *xvid4;
-#endif
-#ifdef USE_DIVX
   GtkWidget *divx;
-#endif
-
   GtkWidget *mjpeg;
-#ifdef USE_MJPEG
-   GtkWidget *vcd;
-   GtkWidget *svcd;
-   GtkWidget *dvd;
-#endif
-GtkWidget *xvcd,*xsvcd;;
+#endif  
   GtkWidget *buttonConfigure;
+  GtkWidget *hbox1;
+  GtkWidget *buttonCalc;
   GtkWidget *dialog_action_area1;
   GtkWidget *cancelbutton1;
   GtkWidget *okbutton1;
@@ -169,12 +177,16 @@ GtkWidget *xvcd,*xsvcd;;
   gtk_widget_show (vbox1);
   gtk_box_pack_start (GTK_BOX (dialog_vbox1), vbox1, TRUE, TRUE, 0);
 
+  hbox2 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox2);
+  gtk_box_pack_start (GTK_BOX (vbox1), hbox2, FALSE, FALSE, 0);
+
   optionmenu_CodecList = gtk_option_menu_new ();
   gtk_widget_show (optionmenu_CodecList);
-  gtk_box_pack_start (GTK_BOX (vbox1), optionmenu_CodecList, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (hbox2), optionmenu_CodecList, FALSE, FALSE, 0);
 
   menu1 = gtk_menu_new ();
-
+#if 0
   ffmpeg_mpeg4 = gtk_menu_item_new_with_mnemonic (_("FFmpeg Mpeg4"));
   gtk_widget_show (ffmpeg_mpeg4);
   gtk_container_add (GTK_CONTAINER (menu1), ffmpeg_mpeg4);
@@ -191,65 +203,31 @@ GtkWidget *xvcd,*xsvcd;;
   gtk_widget_show (ffmpeg_huffyuv);
   gtk_container_add (GTK_CONTAINER (menu1), ffmpeg_huffyuv);
 
-ffmpeg_ffv1 = gtk_menu_item_new_with_mnemonic (_("FFmpeg FFV1"));
-  gtk_widget_show (ffmpeg_ffv1);
-  gtk_container_add (GTK_CONTAINER (menu1), ffmpeg_ffv1);
-  
-  ffmpeg_snow = gtk_menu_item_new_with_mnemonic (_("FFmpeg Snow"));
-  gtk_widget_show (ffmpeg_snow);
-  gtk_container_add (GTK_CONTAINER (menu1), ffmpeg_snow);
-
-
-#ifdef USE_XX_XVID
   xvid = gtk_menu_item_new_with_mnemonic (_("Xvid"));
   gtk_widget_show (xvid);
   gtk_container_add (GTK_CONTAINER (menu1), xvid);
-#endif
-#ifdef USE_XVID_4
-  xvid4 = gtk_menu_item_new_with_mnemonic (_("Xvid API 4"));
-  gtk_widget_show (xvid4);
-  gtk_container_add (GTK_CONTAINER (menu1), xvid4);
-#endif
-#ifdef USE_DIVX
+
   divx = gtk_menu_item_new_with_mnemonic (_("Divx"));
   gtk_widget_show (divx);
   gtk_container_add (GTK_CONTAINER (menu1), divx);
-#endif
-
-
 
   mjpeg = gtk_menu_item_new_with_mnemonic (_("Mjpeg"));
   gtk_widget_show (mjpeg);
   gtk_container_add (GTK_CONTAINER (menu1), mjpeg);
-#ifdef USE_MJPEG
-vcd = gtk_menu_item_new_with_mnemonic (_("VCD"));
-  gtk_widget_show (vcd);
-  gtk_container_add (GTK_CONTAINER (menu1), vcd);
-
-svcd = gtk_menu_item_new_with_mnemonic (_("SVCD"));
-  gtk_widget_show (svcd);
-  gtk_container_add (GTK_CONTAINER (menu1), svcd);
-
-dvd = gtk_menu_item_new_with_mnemonic (_("DVD"));
-  gtk_widget_show (dvd);
-  gtk_container_add (GTK_CONTAINER (menu1), dvd);
-
 #endif
-
-xvcd = gtk_menu_item_new_with_mnemonic (_("XVCD"));
-  gtk_widget_show (xvcd);
-  gtk_container_add (GTK_CONTAINER (menu1), xvcd);
-
-xsvcd = gtk_menu_item_new_with_mnemonic (_("DVD (ffmpeg)"));
-  gtk_widget_show (xsvcd);
-  gtk_container_add (GTK_CONTAINER (menu1), xsvcd);
-
-
   gtk_option_menu_set_menu (GTK_OPTION_MENU (optionmenu_CodecList), menu1);
 
-  buttonConfigure = gtk_button_new_with_mnemonic (_("Configure codec"));
+  buttonConfigure = gtk_button_new_with_mnemonic (_("Configure Codec"));
   gtk_widget_show (buttonConfigure);
-  gtk_box_pack_start (GTK_BOX (vbox1), buttonConfigure, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (hbox2), buttonConfigure, FALSE, FALSE, 0);
+
+  hbox1 = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (hbox1);
+  gtk_box_pack_start (GTK_BOX (vbox1), hbox1, FALSE, FALSE, 0);
+
+  buttonCalc = gtk_button_new_with_mnemonic (_("Calculator"));
+  gtk_widget_show (buttonCalc);
+  gtk_box_pack_end (GTK_BOX (hbox1), buttonCalc, FALSE, FALSE, 0);
 
   dialog_action_area1 = GTK_DIALOG (dialogVideoCodec)->action_area;
   gtk_widget_show (dialog_action_area1);
@@ -269,40 +247,26 @@ xsvcd = gtk_menu_item_new_with_mnemonic (_("DVD (ffmpeg)"));
   GLADE_HOOKUP_OBJECT_NO_REF (dialogVideoCodec, dialogVideoCodec, "dialogVideoCodec");
   GLADE_HOOKUP_OBJECT_NO_REF (dialogVideoCodec, dialog_vbox1, "dialog_vbox1");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, vbox1, "vbox1");
+  GLADE_HOOKUP_OBJECT (dialogVideoCodec, hbox2, "hbox2");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, optionmenu_CodecList, "optionmenu_CodecList");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, menu1, "menu1");
+#if 0  
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, ffmpeg_mpeg4, "ffmpeg_mpeg4");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, ffmpeg_h263, "ffmpeg_h263");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, ffmpeg_h263p, "ffmpeg_h263p");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, ffmpeg_huffyuv, "ffmpeg_huffyuv");
-#ifdef USE_XX_XVID  
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, xvid, "xvid");
-#endif
-#ifdef USE_XVID_4
-  GLADE_HOOKUP_OBJECT (dialogVideoCodec, xvid4, "xvid4");
-#endif
-
-#ifdef USE_DIVX  
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, divx, "divx");
-#endif
-
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, mjpeg, "mjpeg");
-#ifdef USE_MJPEG    
-  GLADE_HOOKUP_OBJECT (dialogVideoCodec, vcd, "vcd");
-  GLADE_HOOKUP_OBJECT (dialogVideoCodec, svcd, "svcd");
-  GLADE_HOOKUP_OBJECT (dialogVideoCodec, dvd, "dvd");
-#endif
-GLADE_HOOKUP_OBJECT (dialogVideoCodec, xvcd, "xvcd");
-GLADE_HOOKUP_OBJECT (dialogVideoCodec, xsvcd, "xsvcd");
-
+#endif  
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, buttonConfigure, "buttonConfigure");
+  GLADE_HOOKUP_OBJECT (dialogVideoCodec, hbox1, "hbox1");
+  GLADE_HOOKUP_OBJECT (dialogVideoCodec, buttonCalc, "buttonCalc");
   GLADE_HOOKUP_OBJECT_NO_REF (dialogVideoCodec, dialog_action_area1, "dialog_action_area1");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, cancelbutton1, "cancelbutton1");
   GLADE_HOOKUP_OBJECT (dialogVideoCodec, okbutton1, "okbutton1");
 
-  	 gtk_signal_connect(GTK_OBJECT(buttonConfigure), "clicked",
-                      GTK_SIGNAL_FUNC(okCallback),                   (void *) 0);
-
   return dialogVideoCodec;
 }
+
 
