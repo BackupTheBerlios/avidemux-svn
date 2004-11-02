e7d32dd9d107faa5ef2a65b243ca7d1347b0dd41
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2004-11-02 19:07:16 +0000

    cleanup : use sample api rather than read api

--- trunk/avidemux/avidemux/ADM_audiofilter/audioeng_rawshift.cpp
+++ trunk/avidemux/avidemux/ADM_audiofilter/audioeng_rawshift.cpp
@@ -2,10 +2,8 @@
                           audioeng_rawshift.cpp  -  description
                              -------------------
 
-  This filter is similar to timeshift but work also on compressed stream
-  (i.e. no need to recompress)                             	
-
-
+ 	This filter i used only in copy mode
+			     
     begin                : Fri Jun 28 2002
     copyright            : (C) 2002 by mean
     email                : fixounet@free.fr
@@ -39,135 +37,86 @@
 // Ctor       `AVDMProcessAudioStream::AVDMProcessAudioStream(AVD
 //__________
 
-AVDMProcessAudio_RawShift::AVDMProcessAudio_RawShift(
-AVDMGenericAudioStream * instream, int32_t msoff,uint32_t starttime):
+AVDMProcessAudio_RawShift::AVDMProcessAudio_RawShift(AVDMGenericAudioStream * instream, int32_t msoff,
+	uint32_t starttime):
 AVDMProcessAudioStream    (instream)
 {
     _wavheader = new WAVHeader;
     memcpy(_wavheader, _instream->getInfo(), sizeof(WAVHeader));
+    
 
-    // first go to the beginiing....
-    _timeoffset=starttime;
-    _instream->goToTime(  _timeoffset);
-	_startoffset=_instream->getPos();
-	 printf("\n** start raw :%lu ",_startoffset);
+    // first go to the begining....
+	_starttime=starttime;
+ 	_msoff=-msoff;
+	_filterOn=0;
+	_sampleOffset=0;
 
- 	msoff=-msoff;
-    _outpos=0;
-
-    if (msoff > 0)
-    {
-      _instream->goToTime(  _timeoffset+msoff);
-	  _byteoffset =_instream->getPos()-_startoffset;
+    	if (_msoff > 0) // just seek in the file
+   	{
+      		_instream->goToTime(  _starttime+_msoff);
+		_filterOn=0; // no need for a filter
   	}
-    else
-    {
-
-       printf("\n** start sync :%lu ",_instream->getPos());
-       _instream->goToTime( _timeoffset-msoff);
-       printf("\n**  end sync :%lu ",_instream->getPos());
-
-		_byteoffset =_instream->getPos()-_startoffset;  		
-  		printf("**** Dup byte offset : %ld",_byteoffset);
-		_byteoffset=-_byteoffset;
-  	  _instream->goToTime(  _timeoffset); 	
-        _outpos=-_byteoffset;
+    	else
+	{
+		// need to dupe the beginning
+		// if there is enough audio
+		if(_starttime>=-_msoff)
+		{
+			_instream->goToTime(  _starttime+_msoff); // just rewind to compensate
+			_filterOn=0; // no need for a filter
+		}
+		else
+		{
+			// we have to dupe a bit
+			int32_t dupe;
+			_instream->goToTime(0);
+			_filterOn=1;
+			dupe=-(_starttime+_msoff);
+			_sampleOffset=(dupe*_wavheader->frequency)/1000;
+		}
 
   	}
 
 
     strcpy(_name, "PROC:RAWS");
     _length = instream->getLength();
-    printf("\n Raw shit : %ld ms, byteoffset = %ld\n",msoff,_byteoffset);
+    printf("\n Raw shit : Start:%lu ms, Sample offset = %lu\n",_starttime,_sampleOffset);
 
 
 };
-
-
-uint32_t AVDMProcessAudio_RawShift::readDecompress(uint32_t len,
-						    uint8_t * buffer)
+//
+//	If filterOn, it means we have to dupe sampleOffset sample, going back to start
+//		and do it again
+//
+uint8_t	 AVDMProcessAudio_RawShift::getPacket(uint8_t *dest, uint32_t *len, uint32_t *samples)
 {
+	uint8_t r;
+		
+		if(!_filterOn)
+			return _instream->getPacket(dest,len,samples);
 
-    uint32_t pos=0;
-
-
-    // no more offset to apply
-    if (_outpos == 0)
-			return _instream->read(len, buffer);
-
- 	 // still in prebuffer
-   if(len<_outpos)
-   	{
-      	
-	   	pos= _instream->read(len, buffer);
-     	_outpos-=pos;
-      printf("\n prebuffering : %lu",_outpos);
-      	return pos;
-     }
-    // need mode or 0
-    // first empty prebuffer
-
-         pos= _instream->read(_outpos, buffer);
-         // then rewind
-         _outpos=0;
-         _instream->goToSync(_startoffset);
-         // and read leftover;
-         return _instream->read( len-pos,buffer+pos);
-
+		// filter is still on
+		r=_instream->getPacket(dest,len,samples);
+		if(!r)
+		{
+			printf("RawShift: Readerror\n");
+			*len=0;
+			*samples=0;
+		}
+		if(_sampleOffset<=*samples)
+		{
+			printf("RawShift : End of prebuffering\n");
+			_filterOn=0;
+			_instream->goToTime(_starttime);
+		}
+		else
+		{
+			_sampleOffset-=*samples;
+			//printf("%lu to go\n",_sampleOffset);
+		}
+		return r;
 
 }
-
-//
-//      We do nothing more that asking the input stream to go at the same place
-//
-//  if _byteoffset is > 0 we go to offset+byteoffset
-// else byteoffset is <0 so it means we duplicate a  [0, byteoffset] seg
-//
-// if  _newoffset is [0,byteoffset] we go to it
-// else we go to newoffset-byteoffset
-//___________________________________________
-
-uint8_t AVDMProcessAudio_RawShift::goToTime(uint32_t newoffset)
-{
- return goTo(convTime2Offset(newoffset))   ;
- }
-
-uint8_t AVDMProcessAudio_RawShift::goTo(uint32_t newoffset)
-{
-
-	if( _byteoffset>0)
- 	{
-
- 	 return _instream->goTo(_startoffset+newoffset+_byteoffset);
-   }
-	 else
-  {
- 			if(  newoffset< (uint32_t)(-_byteoffset))
-    		{
-        		// we need to buffer [0, byteoffset]
-           	int32_t l;
-        	
-        		l=-_byteoffset-newoffset;
-          		_outpos=l;
-        		return _instream->goTo(_startoffset+newoffset);
-          }
-    			else
-       {
-         	_outpos=0; //  no need to duplicate
-       	 return _instream->goTo(_startoffset+newoffset+_byteoffset);
-
-         }
-
-      }
-};
-
-
-AVDMProcessAudio_RawShift::~AVDMProcessAudio_RawShift()
-{
-    delete _wavheader;
-
-}
-
 //
 //      First we read the input buffer and apply the volume transformation
 //  in it.    No need for specific buffer
@@ -178,3 +127,40 @@ uint32_t AVDMProcessAudio_RawShift::read(uint32_t len, uint8_t * buffer)
     return readDecompress(len, buffer);
 
 };
+uint32_t AVDMProcessAudio_RawShift::readDecompress(uint32_t len,
+						    uint8_t * buffer)
+{
+
+  	uint32_t l,sam;
+		if(!getPacket(buffer,&len,&sam))
+			return 0;
+		return len;
+
+}
+
+
+//___________________________________________
+
+uint8_t AVDMProcessAudio_RawShift::goToTime(uint32_t newoffset)
+{
+ 	// since we are in copy mode, the only value accepted is 0 here
+	ADM_assert(!newoffset);
+	return 1;
+}
+
+uint8_t AVDMProcessAudio_RawShift::goTo(uint32_t newoffset)
+{
+
+	// since we are in copy mode, the only value accepted is 0 here
+	ADM_assert(!newoffset);
+	return 1;
+};
+
+
+AVDMProcessAudio_RawShift::~AVDMProcessAudio_RawShift()
+{
+    delete _wavheader;
+
+}
+
+
