3f2bc65226a82f373e001fc2e9ace0806aa516af
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2005-08-13 13:20:26 +0000

    correct header, (c)

--- branches/avidemux_2.1_branch/avidemux/ADM_video/ADM_vidFieldUnblend.cpp
+++ branches/avidemux_2.1_branch/avidemux/ADM_video/ADM_vidFieldUnblend.cpp
@@ -1,9 +1,19 @@
-// Port of Bach unblend avisynth filter
-//
-//      Copyright by Bach, D Graft, Ernst Peché
-//      Probably GPL
-//
-//
+/***************************************************************************
+                          toolkit.hxx  -  description
+                             -------------------
+    begin                : Fri Dec 14 2001
+    copyright            : (C) 2001 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
