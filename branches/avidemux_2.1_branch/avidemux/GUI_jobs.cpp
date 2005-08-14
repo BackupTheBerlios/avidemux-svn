ae315cd856662c646034a66117cef6515918fcad
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2005-08-14 17:16:27 +0000

    jobs 1/2

---
+++ branches/avidemux_2.1_branch/avidemux/GUI_jobs.cpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+    copyright            : (C) 2001/2005 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+
+#include "default.h"
+#include "ADM_assert.h"
+#include "ADM_toolkit/filesel.h"
+#include "ADM_toolkit/toolkit.hxx"
+
+uint8_t GUI_jobs(void)
+{
+        GUI_Alert("Nothing to see here, go away");
+        return 0;
+}
