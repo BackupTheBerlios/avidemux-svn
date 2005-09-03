7bc989f322a5da91df398efc0e8a6a1b615f4754
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2005-09-03 14:35:38 +0000

    Diego Petteno patches

--- branches/avidemux_2.1_branch/avidemux/avi_vars.h
+++ branches/avidemux_2.1_branch/avidemux/avi_vars.h
@@ -19,7 +19,7 @@
 #if (defined( HAVE_LIBESD) && defined(HAVE_ESD_H)) || \
  defined(OSS_SUPPORT) || defined (USE_ARTS) || \
   defined(USE_SDL) || defined(CONFIG_DARWIN) || \
-  defined(CYG_MANGLING)
+  defined(CYG_MANGLING) || defined(ALSA_SUPPORT)
   
 #define HAVE_AUDIO
 #endif
