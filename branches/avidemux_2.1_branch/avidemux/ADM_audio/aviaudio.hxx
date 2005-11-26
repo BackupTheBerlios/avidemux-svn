f02f83366d7bfdded7012e2af094220750d9b49c
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2005-11-26 15:43:50 +0000

    add define for IMA ADPCM

--- branches/avidemux_2.1_branch/avidemux/ADM_audio/aviaudio.hxx
+++ branches/avidemux_2.1_branch/avidemux/ADM_audio/aviaudio.hxx
@@ -73,6 +73,7 @@ typedef struct
 #define WAV_8BITS_UNSIGNED 	55 // dummy id
 #define WAV_AMRNB 	56 // dummy id
 #define WAV_ULAW	57 // dummy id
+#define WAV_IMAADPCM    17
 #include "ADM_audiocodec/ADM_audiocodec.h"
 #include "ADM_library/ADM_fileio.h"
 class AVDMGenericAudioStream
