c17527af950c7cbbd03498b4aeb22db40376774e
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-06-10 20:31:46 +0000

    declare new filter mcdeint

--- branches/avidemux_2.2_branch/avidemux/ADM_filter/filter_declaration.cpp
+++ branches/avidemux_2.2_branch/avidemux/ADM_filter/filter_declaration.cpp
@@ -65,6 +65,8 @@ printf(  "*********************\n");
 	registerFilter("----------- Interlacing -------------",VF_INVALID,2,NULL,NULL);
 	// Buggy : Removed REGISTER("IVTC",VF_IVTC,1,ivtc_create);
 //	REGISTERX("mpivtc","Mplayer ivtc",VF_MPDETC,1,mpdetc_create,mpdetc_script);	
+
+        REGISTERX("mcdeinterlace","mcDeinterlace","Motion Compensation Deinterlaced.\nPort of M. Niedermayer filter present in MPlayer.",VF_MCDEINT,1,mcdeint_create,mcdeint_script);
 	REGISTERX("deinterlace","Deinterlace","Mask interlacing artifacts. Port of Smart deinterlace.",VF_DEINTERLACE,1,deinterlace_create,deinterlace_script);
 	REGISTERX("kerneldeint","KernelDeint","Kernel deinterlacer by Donald Graft.",VF_KRNDEINT,1,kerneldeint_create,kerneldeint_script);
         REGISTERX("tdeint","TDeint","Motion adaptative deinterlacer by Tritical.",VF_TDEINT,1,tdeint_create,tdeint_script);
