cf3567406287ec67c5516ac5db21dd3266ee1f64
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-06-10 10:47:08 +0000

    separete header

---
+++ branches/avidemux_2.2_branch/avidemux/ADM_mplex/streamType.h
@@ -0,0 +1,15 @@
+#ifndef STREAM_TYPE_H
+#define STREAM_TYPE_H
+enum StreamKind
+  {
+      MPEG_AUDIO=1,
+      AC3_AUDIO,
+      LPCM_AUDIO,
+      DTS_AUDIO,
+      MPEG_VIDEO
+#ifdef ZALPHA
+        ,
+      Z_ALPHA
+#endif
+  };
+#endif
