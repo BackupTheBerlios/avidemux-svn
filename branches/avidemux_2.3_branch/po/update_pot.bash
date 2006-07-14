49693c4483026b63022440e4fe6d3bd9480237f8
mihailzenkov <mihailzenkov@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-07-14 15:40:57 +0000

    fix in po

--- branches/avidemux_2.3_branch/po/update_pot.bash
+++ branches/avidemux_2.3_branch/po/update_pot.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-xgettext --keyword=_ --debug --from-code=en -C -D .. -p . -f POTFILES.in -o avidemux.pot
+xgettext --keyword=_ --debug --from-code=utf-8 -C -D .. -p . -f POTFILES.in -o avidemux.pot
 echo "DONE."
