b666473b3f5ddbfbb8acbc13cd2b05f4b78deba8
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-09-03 15:54:12 +0000

    int2float, separate for assembly later

---
+++ branches/avidemux_2.3_branch/avidemux/ADM_toolkit/ADM_intfloat.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+                 
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ADM_library/default.h"
+#include "ADM_cpuCap.h"
+
+#include "ADM_assert.h"
+
+#include "ADM_intfloat.h"
+
+static uint8_t ADM_int2float_C(int16_t *in, float *out,int nb);
+static uint8_t ADM_float2int_C(float *in,int16_t *out,int nb);
+
+uint8_t ADM_intFloatInit(void)
+{
+  pADM_int2float=&ADM_int2float_C;
+  pADM_float2int=&ADM_float2int_C;
+  return 1;
+}
+uint8_t ADM_int2float_C(int16_t *in, float *out,int nb)
+{
+  return 0;
+  
+}
+uint8_t ADM_float2int_C(float *in,int16_t *out,int nb)
+{
+  return 0;
+  
+}
+
