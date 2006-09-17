c8feb9e4ff8db4bf67c11eaa70c1d56a143494ee
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-09-17 13:34:09 +0000

    more prototyping

--- branches/avidemux_2.3_branch/avidemux/ADM_library/avidemutils.h
+++ branches/avidemux_2.3_branch/avidemux/ADM_library/avidemutils.h
@@ -10,6 +10,7 @@
 uint8_t  ADM_findMpegStartCode(uint8_t *start, uint8_t *end,uint8_t *outstartcode,uint32_t *offset);
 uint32_t ADM_computeBitrate(uint32_t fps1000, uint32_t nbFrame, uint32_t sizeInMB);
 ADM_ASPECT getAspectRatioFromAR(uint32_t width, uint32_t height,const char **s);
+char *ADM_escape(const ADM_filename *incoming);
 
 #endif
 //EOF
