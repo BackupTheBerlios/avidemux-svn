3dc9d1e83b043cf65829a187494a533dde0be8a2
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-11-01 16:07:19 +0000

    moving

--- branches/avidemux_2.4_branch/avidemux/libcheck.sh
+++ branches/avidemux_2.4_branch/avidemux/libcheck.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find . -name "*.a" | xargs nm -AC | grep "gtk_\|fread\|fwrite\|malloc"| grep   " U " | grep -v ".* U .*_.*"
+echo "Checking Deps..."
+find . -name "*.a" | xargs nm -AC | grep "fread\|fwrite\|malloc"| grep   " U " | grep -v ".* U .*_.*"
+echo "Checking GTK..."
+find . -name "*.a" | grep -vi gtk | xargs nm -AC | grep "gtk_"| grep   " U " 
