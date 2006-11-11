59b7bdf9e9c2428632e8da169eafc5e30b76191c
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-11-11 13:49:28 +0000

    updated chromaShift ui

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_GTK/ADM_dialog/DIA_chromaShift.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_GTK/ADM_dialog/DIA_chromaShift.cpp
@@ -55,6 +55,7 @@
 
 #include "ADM_colorspace/ADM_rgb.h"
 #include "ADM_video/ADM_vidChromaShift.h"
+#include "ADM_video/ADM_vidChromaShift_param.h"
 
 #include "ADM_osSupport/ADM_debugID.h"
 #define MODULE_NAME MODULE_FILTER
@@ -67,72 +68,97 @@
 #define SPIN_GET(x,y) {y= gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(lookup_widget(dialog,#x))) ;\
 				printf(#x":%d\n", y);}
 
-#define SPIN_SET(x,y)  {gtk_spin_button_set_value(GTK_SPIN_BUTTON(lookup_widget(dialog,#x)),(gfloat)y) ; \
-				printf(#x":%d\n", y);}
+#define SPIN_SET(x,y)  {gtk_spin_button_set_value(GTK_SPIN_BUTTON(lookup_widget(dialog,#x)),(gfloat)y) ; printf(#x":%d\n", y);}
 
 
-static uint8_t *video_working,*video_rgb;
-static	ADMImage *video_src;
 static int32_t shift_u, shift_v;
 static uint32_t ww,hh;
 
 static GtkWidget *dialog;
 static GtkWidget *create_ChromaShift( void );
-static uint8_t DIA_getChromaShift( void );
+
+
 static void chromadraw (GtkWidget *dialog,uint32_t w,uint32_t h );
 static void read( void );
 static void upload ( void );
 static gboolean gui_draw( void );
 static gboolean gui_update( void );
+static gboolean slider_update( void );
 static void update(void);
+static int curImage=0;
+//**************************************
+static ADMImage   *video_src=NULL;
+static ADMImage   *video_shifted=NULL;
+static uint8_t    *video_rgb=NULL;
+static ColYuvRgb  *rgbConv=NULL;
+AVDMGenericVideoStream *sstream=NULL;
 
-static ColYuvRgb    *rgbConv=NULL;
-
-uint8_t DIA_getChromaShift( uint8_t *work, uint8_t *rgb,ADMImage *src,ColYuvRgb    *rgbC,uint32_t *u,uint32_t *v );
-uint8_t DIA_getChromaShift( uint8_t *work, uint8_t *rgb,ADMImage *src,ColYuvRgb    *rgbC,
-                              uint32_t *u,uint32_t *v )
+uint8_t DIA_getChromaShift( AVDMGenericVideoStream *instream,CHROMASHIFT_PARAM    *param );
+uint8_t DIA_getChromaShift( AVDMGenericVideoStream *instream,CHROMASHIFT_PARAM    *param )
 {
-	uint8_t ret=0;
-        video_working=work;
-        video_rgb=rgb;
-        video_src=src;
-        rgbConv=rgbC;
-        shift_u=*u;
-        shift_v=*v;
+uint8_t ret=0;
+        sstream=instream;
+        ww=instream->getInfo()->width;
+        hh=instream->getInfo()->height;
+        video_src=new ADMImage(ww,hh);
+        video_shifted=new ADMImage(ww,hh);
+        video_rgb=new uint8_t[ww*hh*4];
+        rgbConv=new ColYuvRgb(ww,hh);
+        rgbConv->reset(ww,hh);
+        shift_u=param->u;
+        shift_v=param->v;
 	dialog=create_ChromaShift();
 	gtk_register_dialog(dialog);
-	gtk_widget_set_usize(WID(drawingarea1), ww,hh);
-	gtk_widget_show(dialog);
-	upload();
-	 gtk_signal_connect(GTK_OBJECT(WID(drawingarea1)), "expose_event",
-		       GTK_SIGNAL_FUNC(gui_draw),
-		       NULL);
- 	gtk_signal_connect (GTK_OBJECT(WID( spinbutton_U)), "value_changed",
+        curImage=0;
+        
+        gtk_widget_set_usize(WID(drawingarea1), ww,hh);
+        gtk_widget_show(dialog);
+        upload();
+        gtk_signal_connect(GTK_OBJECT(WID(drawingarea1)), "expose_event",
+                        GTK_SIGNAL_FUNC(gui_draw),
+                        NULL);
+        gtk_signal_connect (GTK_OBJECT(WID( spinbutton_U)), "value_changed",
                     GTK_SIGNAL_FUNC (gui_update),
                     NULL);
-	gtk_signal_connect (GTK_OBJECT(WID( spinbutton_V)), "value_changed",
+        gtk_signal_connect (GTK_OBJECT(WID( spinbutton_V)), "value_changed",
                     GTK_SIGNAL_FUNC (gui_update),
                     NULL);
-
-	update();
-
-	int response;
-	while( (response=gtk_dialog_run(GTK_DIALOG(dialog)))==GTK_RESPONSE_APPLY)
-	{
-		read();
-		update();
-	}
-	if(response==GTK_RESPONSE_OK)
+        gtk_signal_connect (GTK_OBJECT(WID( spinbutton_V)), "value_changed",
+                    GTK_SIGNAL_FUNC (gui_update),
+                    NULL);
+         gtk_signal_connect (GTK_OBJECT(WID( hscale)), "value_changed",
+                    GTK_SIGNAL_FUNC (slider_update),
+                    NULL);
+        uint32_t len,flags;
+        if(!instream->getFrameNumberNoAlloc(curImage,&len,video_src,&flags))
         {
-		read();
-                *u=shift_u;
-                *v=shift_v;
-		ret=1;
-	}
-	gtk_unregister_dialog(dialog);
-	gtk_widget_destroy(dialog);
-	dialog=NULL;
-	return ret;
+          GUI_Error_HIG("Frame error","Cannnot read frame to display");
+          return 0;
+        }
+         
+        update();
+  
+        int response;
+        while( (response=gtk_dialog_run(GTK_DIALOG(dialog)))==GTK_RESPONSE_APPLY)
+        {
+                read();
+                update();
+        }
+        if(response==GTK_RESPONSE_OK)
+        {
+                read();
+                param->u=shift_u;
+                param->v=shift_v;
+                ret=1;
+        }
+        gtk_unregister_dialog(dialog);
+        gtk_widget_destroy(dialog);
+        dialog=NULL;
+        delete video_src;
+        delete video_shifted;
+        delete [] video_rgb;
+        delete rgbConv;
+        return ret;
 
 }
 void read( void )
@@ -151,23 +177,21 @@ void upload ( void )
 void update( void)
 {
 
-		// First copy Y
-		memcpy(video_working,video_src->data,(ww*hh));
-		// then shift u
+        // First copy Y
+        memcpy(video_shifted->data,video_src->data,(ww*hh));
+        // then shift u
 
-		 ADMVideoChromaShift::shift(video_working+ww*hh,
-		 						video_src->data+ww*hh, ww>>1,hh>>1,shift_u);
-		ADMVideoChromaShift::shift(video_working+((5*ww*hh)>>2),
-		 						video_src->data+((5*ww*hh)>>2), ww>>1,hh>>1,shift_v);
-		if(shift_u)
-			ADMVideoChromaShift::fixup(video_working,ww,hh,shift_u*2);
-		if(shift_v)
-			ADMVideoChromaShift::fixup(video_working,ww,hh,shift_v*2);
+         ADMVideoChromaShift::shift(video_shifted->data+ww*hh,
+                                    video_src->data+ww*hh, ww>>1,hh>>1,shift_u);
+        ADMVideoChromaShift::shift(video_shifted->data+((5*ww*hh)>>2),
+                                    video_src->data+((5*ww*hh)>>2), ww>>1,hh>>1,shift_v);
+        if(shift_u)
+                ADMVideoChromaShift::fixup(video_shifted->data,ww,hh,shift_u*2);
+        if(shift_v)
+                ADMVideoChromaShift::fixup(video_shifted->data,ww,hh,shift_v*2);
 
-		//COL_yv12rgb(ww,hh,video_working,video_rgb);
-		rgbConv->scale(video_working,video_rgb);
-		chromadraw(dialog,ww,hh);
-	//	printf("\n Updated..\n");
+        rgbConv->scale(video_shifted->data,video_rgb);
+        chromadraw(dialog,ww,hh);
 }
 gboolean gui_update( void )
 {
@@ -175,6 +199,26 @@ gboolean gui_update( void )
 	read();
 	update();
 	return true;
+}
+gboolean slider_update( void )
+{
+  GtkAdjustment *adj;   
+        printf("Slider update\n");
+        adj=gtk_range_get_adjustment (GTK_RANGE(WID(hscale)));
+        double val=(double)GTK_ADJUSTMENT(adj)->value;
+        
+        val=(val*sstream->getInfo()->nb_frames)/100.;
+        curImage=(uint32_t)floor(val);
+        
+        uint32_t len,flags;
+        if(!sstream->getFrameNumberNoAlloc(curImage,&len,video_src,&flags))
+        {
+          printf("Frame %u / %u\n",curImage,sstream->getInfo()->nb_frames);
+          GUI_Error_HIG("Frame error","Cannnot read frame to display");
+          return true;
+        }
+        gui_update();
+        return true;
 }
 
 /*---------------------------------------------------------------------------
@@ -192,7 +236,9 @@ void chromadraw (GtkWidget *dialog,uint32_t w,uint32_t h )
 	GUI_RGBDisplay(video_rgb, w,h, (void *)draw);
 }
 /*----------------------------------------------------------------*/
-GtkWidget		*create_ChromaShift (void)
+
+GtkWidget*
+create_ChromaShift (void)
 {
   GtkWidget *ChromaShift;
   GtkWidget *dialog_vbox1;
@@ -204,7 +250,11 @@ GtkWidget		*create_ChromaShift (void)
   GtkWidget *spinbutton_U;
   GtkObject *spinbutton_V_adj;
   GtkWidget *spinbutton_V;
+  GtkWidget *hscale;
+  GtkWidget *frame1;
+  GtkWidget *alignment1;
   GtkWidget *drawingarea1;
+  GtkWidget *label3;
   GtkWidget *dialog_action_area1;
   GtkWidget *cancelbutton1;
   GtkWidget *applybutton1;
@@ -212,6 +262,7 @@ GtkWidget		*create_ChromaShift (void)
 
   ChromaShift = gtk_dialog_new ();
   gtk_window_set_title (GTK_WINDOW (ChromaShift), _("ChromaShift"));
+  gtk_window_set_type_hint (GTK_WINDOW (ChromaShift), GDK_WINDOW_TYPE_HINT_DIALOG);
 
   dialog_vbox1 = GTK_DIALOG (ChromaShift)->vbox;
   gtk_widget_show (dialog_vbox1);
@@ -222,14 +273,13 @@ GtkWidget		*create_ChromaShift (void)
 
   table1 = gtk_table_new (2, 2, FALSE);
   gtk_widget_show (table1);
-  gtk_box_pack_start (GTK_BOX (vbox1), table1, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (vbox1), table1, FALSE, FALSE, 0);
 
   label1 = gtk_label_new (_("U Shift :"));
   gtk_widget_show (label1);
   gtk_table_attach (GTK_TABLE (table1), label1, 0, 1, 0, 1,
                     (GtkAttachOptions) (GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
   gtk_misc_set_alignment (GTK_MISC (label1), 0, 0.5);
 
   label2 = gtk_label_new (_("V Shift :"));
@@ -237,14 +287,13 @@ GtkWidget		*create_ChromaShift (void)
   gtk_table_attach (GTK_TABLE (table1), label2, 0, 1, 1, 2,
                     (GtkAttachOptions) (GTK_FILL),
                     (GtkAttachOptions) (0), 0, 0);
-  gtk_label_set_justify (GTK_LABEL (label2), GTK_JUSTIFY_LEFT);
   gtk_misc_set_alignment (GTK_MISC (label2), 0, 0.5);
 
   spinbutton_U_adj = gtk_adjustment_new (0, -32, 32, 1, 10, 10);
   spinbutton_U = gtk_spin_button_new (GTK_ADJUSTMENT (spinbutton_U_adj), 1, 0);
   gtk_widget_show (spinbutton_U);
   gtk_table_attach (GTK_TABLE (table1), spinbutton_U, 1, 2, 0, 1,
-                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0),
                     (GtkAttachOptions) (0), 0, 0);
   gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinbutton_U), TRUE);
 
@@ -252,14 +301,33 @@ GtkWidget		*create_ChromaShift (void)
   spinbutton_V = gtk_spin_button_new (GTK_ADJUSTMENT (spinbutton_V_adj), 1, 0);
   gtk_widget_show (spinbutton_V);
   gtk_table_attach (GTK_TABLE (table1), spinbutton_V, 1, 2, 1, 2,
-                    (GtkAttachOptions) (GTK_EXPAND | GTK_FILL),
+                    (GtkAttachOptions) (0),
                     (GtkAttachOptions) (0), 0, 0);
   gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinbutton_V), TRUE);
 
+  hscale = gtk_hscale_new (GTK_ADJUSTMENT (gtk_adjustment_new (0, 0, 100, 1, 1, 1)));
+  gtk_widget_show (hscale);
+  gtk_box_pack_start (GTK_BOX (vbox1), hscale, FALSE, FALSE, 0);
+  gtk_scale_set_digits (GTK_SCALE (hscale), 0);
+
+  frame1 = gtk_frame_new (NULL);
+  gtk_widget_show (frame1);
+  gtk_box_pack_start (GTK_BOX (vbox1), frame1, TRUE, TRUE, 0);
+  gtk_frame_set_shadow_type (GTK_FRAME (frame1), GTK_SHADOW_NONE);
+
+  alignment1 = gtk_alignment_new (0.5, 0.5, 1, 1);
+  gtk_widget_show (alignment1);
+  gtk_container_add (GTK_CONTAINER (frame1), alignment1);
+  gtk_alignment_set_padding (GTK_ALIGNMENT (alignment1), 0, 0, 12, 0);
+
   drawingarea1 = gtk_drawing_area_new ();
   gtk_widget_show (drawingarea1);
-  gtk_box_pack_start (GTK_BOX (vbox1), drawingarea1, TRUE, TRUE, 0);
-  gtk_widget_set_size_request (drawingarea1, 512, 384);
+  gtk_container_add (GTK_CONTAINER (alignment1), drawingarea1);
+
+  label3 = gtk_label_new (_("<b>Preview</b>"));
+  gtk_widget_show (label3);
+  gtk_frame_set_label_widget (GTK_FRAME (frame1), label3);
+  gtk_label_set_use_markup (GTK_LABEL (label3), TRUE);
 
   dialog_action_area1 = GTK_DIALOG (ChromaShift)->action_area;
   gtk_widget_show (dialog_action_area1);
@@ -289,7 +357,11 @@ GtkWidget		*create_ChromaShift (void)
   GLADE_HOOKUP_OBJECT (ChromaShift, label2, "label2");
   GLADE_HOOKUP_OBJECT (ChromaShift, spinbutton_U, "spinbutton_U");
   GLADE_HOOKUP_OBJECT (ChromaShift, spinbutton_V, "spinbutton_V");
+  GLADE_HOOKUP_OBJECT (ChromaShift, hscale, "hscale");
+  GLADE_HOOKUP_OBJECT (ChromaShift, frame1, "frame1");
+  GLADE_HOOKUP_OBJECT (ChromaShift, alignment1, "alignment1");
   GLADE_HOOKUP_OBJECT (ChromaShift, drawingarea1, "drawingarea1");
+  GLADE_HOOKUP_OBJECT (ChromaShift, label3, "label3");
   GLADE_HOOKUP_OBJECT_NO_REF (ChromaShift, dialog_action_area1, "dialog_action_area1");
   GLADE_HOOKUP_OBJECT (ChromaShift, cancelbutton1, "cancelbutton1");
   GLADE_HOOKUP_OBJECT (ChromaShift, applybutton1, "applybutton1");
--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_NONE/ADM_dialog/DIA_none.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_NONE/ADM_dialog/DIA_none.cpp
@@ -56,6 +56,7 @@
 #include "ADM_video/ADM_vidHue.h"
 #include "ADM_video/ADM_vobsubinfo.h"
 #include "ADM_video/ADM_vidVobSub.h"
+#include "ADM_video/ADM_vidChromaShift_param.h"
 #include "ADM_video/ADM_vidMPdelogo.h"
 #include "ADM_video/ADM_vidEq2.h"
 #include "ADM_video/ADM_lavpp_deintparam.h"
@@ -159,7 +160,7 @@ uint8_t DIA_kerneldeint(uint32_t *order, uint32_t *threshold, uint32_t *sharp,
                           uint32_t *twoway, uint32_t *map){return 0;}
 uint8_t DIA_4entries(char *title,uint32_t *left,uint32_t *right,uint32_t *top,uint32_t *bottom) {return 0;}
 uint8_t DIA_videoCodec (SelectCodecType * codec){return 0;}
-uint8_t DIA_getChromaShift( uint8_t *work, uint8_t *rgb,ADMImage *src,ColYuvRgb    *rgbC,uint32_t *u,uint32_t *v ) { return 0;}
+uint8_t DIA_getChromaShift( AVDMGenericVideoStream *instream,CHROMASHIFT_PARAM    *param ){ return 0;}  
 uint8_t DIA_audioCodec( AUDIOENCODER *codec ) {return 0;}
 uint8_t DIA_resizeWiz(RESWIZ_FORMAT *format, RESWIZ_AR *source, RESWIZ_AR *destination) {return 0;}
 uint8_t DIA_dnr(uint32_t *llock,uint32_t *lthresh, uint32_t *clock,
