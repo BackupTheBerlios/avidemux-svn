178348c0406e3a029b581b9a602d6699b95335ff
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-11-19 09:44:28 +0000

    added workish rules

---
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_gui/Q_gui2.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************
+    copyright            : (C) 2001 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "ui_gui2.h"
+
+ class MainWindow : public QMainWindow
+ {
+     Q_OBJECT
+
+ public:
+     MainWindow();
+
+ private slots:
+   
+
+ private:
+     Ui_MainWindow ui;
+ };
+ 
+extern int automation(void );
+
+
+
+MainWindow::MainWindow()     : QMainWindow()
+ {
+     ui.setupUi(this);
+ }
+//*********************************************
+//***** Hook to core                ***********
+//*********************************************
+extern int global_argc;
+extern char **global_argv;
+int UI_Init(int nargc,char **nargv)
+{
+  global_argc=nargc;
+  global_argv=nargv;
+  return 0;
+}
+
+int UI_RunApp(void)
+{
+
+   QApplication a( global_argc, global_argv );
+   MainWindow * mw = new MainWindow();
+    
+    a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
+    mw->show();
+    return a.exec();
+/* if (global_argc >= 2)
+    {
+     automation();
+    }*/
+#include "Q_gui2.moc"
+//EOF
