f9b56bec42b66f087e46120aed8440b0e615df58
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-11-29 19:40:40 +0000

    remember last read from/saved to  dir + factorize

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_gui/file_qt4.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_gui/file_qt4.cpp
@@ -12,64 +12,92 @@
 #include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include "default.h"
 #include <QtCore/QVariant>
 #include <QtGui/qfiledialog.h>
 
 #include "ADM_toolkit/toolkit.hxx"
 #include "ADM_toolkit/filesel.h"
+#include "prefs.h"
 #include "ADM_assert.h"
     
 extern QWidget *QuiMainWindows;
+static void GUI_FileSelSelect(const char *label, char **name, uint32_t access) ;
 //****************************************************************************************************
 void GUI_FileSelRead(const char *label,SELFILE_CB cb) 
 {
-  QString fileName = QFileDialog::getOpenFileName(QuiMainWindows);
-  if(!fileName.isNull() )
+  char *name;
+  GUI_FileSelRead(label,&name);
+  if(name)
   {
-    char *s=ADM_strdup(fileName.toLatin1()); // Fixme utf8 ?
-    
-    printf("File selected :%s\n",s); 
-    cb(s);
-    ADM_dealloc(s);
+    cb(name); 
+    ADM_dealloc(name);
   }
 }
 //****************************************************************************************************
 void GUI_FileSelWrite(const char *label,SELFILE_CB cb) 
 {
-  QString fileName = QFileDialog::getSaveFileName(QuiMainWindows);
-  if(!fileName.isNull() )
+  char *name;
+  GUI_FileSelWrite(label,&name);
+  if(name)
   {
-    char *s=ADM_strdup(fileName.toLatin1()); // Fixme utf8 ?
-    printf("File selected :%s\n",s); 
-    cb(s);
-    ADM_dealloc(s);
+    cb(name); 
+    ADM_dealloc(name);
   }
 
 }
 //****************************************************************************************************
 void GUI_FileSelRead(const char *label, char * * name)
 {
-  *name=NULL;
-  QString fileName = QFileDialog::getOpenFileName(QuiMainWindows);
-  if(!fileName.isNull() )
-  {
-    const char *s=fileName.toLatin1(); // Fixme utf8 ?
-    *name=ADM_strdup(s);
-  }
-
+ GUI_FileSelSelect(label, name, 0) ;
 }
 //****************************************************************************************************
 void GUI_FileSelWrite(const char *label, char * * name)
 {
+ GUI_FileSelSelect(label, name, 1) ;
+
+}
+/**
+  \fn GUI_FileSelWrite (const char *label, char **name, uint32_t access) 
+  \brief Ask for a file for reading (access=0) or writing (access=1)
+
+*/
+void GUI_FileSelSelect(const char *label, char **name, uint32_t access) 
+{
+  char *dir=NULL,*tmpname=NULL;
   *name=NULL;
-  QString fileName = QFileDialog::getSaveFileName(QuiMainWindows);
+  char *str=NULL;
+  options pref_entry=LASTDIR_READ;
+  if(access) pref_entry=LASTDIR_WRITE;
+  
+  if(prefs->get(pref_entry,(ADM_filename **)&tmpname))
+  {
+     DIR *dir;
+        str=PathCanonize(tmpname);
+        PathStripName(str);
+        /* LASTDIR may have gone; then do nothing and use current dir instead (implied) */
+        if( (dir=opendir(str)) )
+        {
+                closedir(dir);
+        } else 
+        {
+            delete [] str;
+            str=NULL;
+        }
+  }
+  QString fileName;
+    if(access)  fileName=QFileDialog::getSaveFileName(QuiMainWindows,label,str);
+        else    fileName=QFileDialog::getOpenFileName(QuiMainWindows,label,str);
+                
   if(!fileName.isNull() )
   {
     const char *s=fileName.toLatin1(); // Fixme utf8 ?
     *name=ADM_strdup(s);
+    prefs->set(pref_entry,(ADM_filename *)*name);
   }
-
+  if(str) delete [] str;
+  
 }
 //****************************************************************************************************
 //EOF
