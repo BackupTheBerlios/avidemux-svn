817c483b5bf6d71f3b3a6f6712419a14c5df514f
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2006-12-06 18:37:15 +0000

    Try to fix build system, ugly evil hack

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_gui/Q_preview.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_gui/Q_preview.cpp
@@ -178,5 +178,5 @@ uint8_t renderStopPlaying( void )
 }
 
 //****************************************************************************************************
-#include "preview_qt4.moc"
+//#include "Q_preview.moc"
 //EOF 
