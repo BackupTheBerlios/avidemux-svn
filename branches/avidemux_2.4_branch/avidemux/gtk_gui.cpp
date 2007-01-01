2788e3b214de2e8d0bcf2eaa7eb0ec9e2c59c411
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-01-01 18:17:37 +0000

    prepare for PSP/H264 profile (not working)

--- branches/avidemux_2.4_branch/avidemux/gtk_gui.cpp
+++ branches/avidemux_2.4_branch/avidemux/gtk_gui.cpp
@@ -479,6 +479,7 @@ int nw;
         case ACT_AUTO_SVCD:
         case ACT_AUTO_DVD:
         case ACT_AUTO_PSP:
+        case ACT_AUTO_PSP_H264:
                 A_autoDrive( action);
                 break;
      case ACT_TimeShift:
