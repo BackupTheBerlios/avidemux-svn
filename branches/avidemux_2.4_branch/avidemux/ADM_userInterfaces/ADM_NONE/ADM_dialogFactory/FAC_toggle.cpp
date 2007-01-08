e47aed19acf6a6ee77fe1be148e9eec639a4d3c3
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-01-08 21:29:24 +0000

    stub for dialogFactory

---
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_NONE/ADM_dialogFactory/FAC_toggle.cpp
@@ -0,0 +1,46 @@
+/***************************************************************************
+  FAC_toggle.cpp
+  Handle dialog factory element : Toggle
+  (C) 2006 Mean Fixounet@free.fr 
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <config.h>
+
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "default.h"
+#include "ADM_commonUI/DIA_factory.h"
+#include "ADM_assert.h"
+
+
+
+
+diaElemToggle::diaElemToggle(uint32_t *toggleValue,const char *toggleTitle, const char *tip)
+  : diaElem(ELEM_TOGGLE)
+{
+}
+
+diaElemToggle::~diaElemToggle()
+{
+  
+}
+void diaElemToggle::setMe(void *dialog, void *opaque)
+{
+}
+void diaElemToggle::getMe(void)
+{
+}
+
+//EOF
---
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/DIA_dialogFactory.cpp
@@ -0,0 +1,40 @@
+/***************************************************************************
+  DIA_dialogFactory.cpp
+  (C) 2006 Mean Fixounet@free.fr 
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <config.h>
+
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "default.h"
+#include "ADM_commonUI/DIA_factory.h"
+#include "ADM_assert.h"
+
+/**
+    \fn diaFactoryRun(const char *title,uint32_t nb,diaElem **elems)
+    \brief  Run a dialog made of nb elems, each elem being described in the **elems
+    @return 0 on failure, 1 on success
+*/
+
+uint8_t diaFactoryRun(const char *title,uint32_t nb,diaElem **elems)
+{
+  return 0;
+  
+}
+
+
+
+//EOF
