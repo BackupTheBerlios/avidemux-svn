b159173d0e5927ac8c74ded6d00a565e7ca2ed1d
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-01-12 19:00:30 +0000

    oops

--- branches/avidemux_2.4_branch/avidemux/ADM_videoFilter/ADM_vidMPLD3Dlow.h
+++ branches/avidemux_2.4_branch/avidemux/ADM_videoFilter/ADM_vidMPLD3Dlow.h
@@ -21,7 +21,7 @@ Daniel Moreno <comac@comac.darktech.org>
 #ifndef __D3DLOW__
 #define __D3DLOW__
 #include "ADM_video/ADM_vidCached.h"
-#include "ADM_video/ADM_vidMPLD3D.h"
+#include "ADM_vidMPLD3D.h"
 
 class  ADMVideoMPD3Dlow:public AVDMGenericVideoStream
  {
