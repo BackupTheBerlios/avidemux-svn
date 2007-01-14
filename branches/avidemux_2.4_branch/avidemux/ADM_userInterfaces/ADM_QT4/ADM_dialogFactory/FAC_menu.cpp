9bb24d3ecf041cc94670cac63994289bc8cef6d6
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-01-14 16:21:44 +0000

    menu for QT4

---
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/FAC_menu.cpp
@@ -0,0 +1,87 @@
+/***************************************************************************
+  FAC_toggle.cpp
+  Handle dialog factory element : Toggle
+  (C) 2006 Mean Fixounet@free.fr 
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <config.h>
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <QDialog>
+#include <QComboBox>
+#include <QLabel>
+#include <QGridLayout>
+
+#include "default.h"
+#include "ADM_commonUI/DIA_factory.h"
+#include "ADM_assert.h"
+
+#include <QCheckBox>
+
+
+
+
+diaElemMenu::diaElemMenu(uint32_t *intValue,const char *itle, uint32_t nb, 
+               const diaMenuEntry *menu,const char *tip)
+  : diaElem(ELEM_MENU)
+{
+  param=(void *)intValue;
+  paramTitle=itle;
+  this->tip=tip;
+  this->menu=menu;
+  this->nbMenu=nb;
+}
+
+diaElemMenu::~diaElemMenu()
+{
+  
+}
+void diaElemMenu::setMe(void *dialog, void *opaque,uint32_t line)
+{
+  QComboBox *combo=new QComboBox( (QWidget *)dialog);
+  QGridLayout *layout=(QGridLayout*) opaque;
+     myWidget=(void *)combo; 
+
+  QLabel *text=new QLabel( this->paramTitle,(QWidget *)dialog);
+  
+  // Fill in combobox
+  int mem=0;
+  const diaMenuEntry *entries=menu;
+  for(int i=0;i<nbMenu;i++)
+  {
+    combo->addItem(entries[i].text);
+    if( *(uint32_t *)param==entries[i].val) mem=i;
+  }
+   combo->setCurrentIndex(mem);
+   layout->addWidget(text,line,0);
+   layout->addWidget(combo,line,1);
+}
+
+void diaElemMenu::getMe(void)
+{
+  QComboBox *combo=(QComboBox *)myWidget;
+  int r;
+  r=combo->currentIndex();
+  ADM_assert(r<nbMenu);
+  *(uint32_t *)param=menu[r].val;
+      
+}
+
+
+
+
+//EOF
