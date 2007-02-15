fe925d091bf9f7f2179282f27502e6763115cdf0
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-02-15 08:00:13 +0000

    use new bitrate widge to redo SVCD/DVD dialog (mpeg2enc based)

--- branches/avidemux_2.4_branch/avidemux/ADM_libraries/ADM_libmpeg2enc/ADM_mpeg2Param.h
+++ branches/avidemux_2.4_branch/avidemux/ADM_libraries/ADM_libmpeg2enc/ADM_mpeg2Param.h
@@ -6,12 +6,12 @@
 
 typedef struct Mpeg2encParam
 {	
-	unsigned int	maxBitrate;
-	unsigned int	gop_size;	
-	unsigned int	widescreen;
-	unsigned int	user_matrix;
-	unsigned int	interlaced;
-	unsigned int	bff;
+	uint32_t	maxBitrate;
+	uint32_t        gop_size;	
+	uint32_t        widescreen;
+	uint32_t        user_matrix;
+	uint32_t        interlaced;
+	uint32_t        bff;
 
 }Mpeg2encParam;
 
---
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_commonUI/DIA_SVCD.cpp
@@ -0,0 +1,83 @@
+//
+//
+// C++ Implementation: DIA_SVCD
+//
+// Description: 
+//
+//
+// Author: mean <fixounet@free.fr>, (C) 2003
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include <config.h>
+//#include "ADM_lavcodec.h"
+
+
+#include <string.h>
+#include <stdio.h>
+
+# include <math.h>
+
+#include "default.h"
+
+#include "ADM_codecs/ADM_codec.h"
+#include "ADM_encoder/ADM_vidEncode.hxx"
+#include "ADM_assert.h" 
+#include "DIA_factory.h"
+#include "../../ADM_libraries/ADM_libmpeg2enc/ADM_mpeg2enc.h"
+
+/**
+      \fn DIA_SVCDParam
+      \brief Dialog to set encoding options for SVCD/DVD mpeg2enc based
+*/
+uint8_t DIA_SVCDParam(COMPRES_PARAMS *incoming)
+{	
+
+Mpeg2encParam *conf=(Mpeg2encParam *)incoming->extraSettings;
+ADM_assert(incoming->extraSettingsLen==sizeof(Mpeg2encParam));
+
+diaMenuEntry wideM[]={
+  {0,_("4:3")},
+  {1,_("16:9")}};
+diaMenuEntry matrixM[]={
+  {0,_("Default")},
+  {1,_("Tmpgenc")},
+  {2,_("Anime")},
+  {3,_("KVCD")}
+};
+diaMenuEntry interM[]={
+  {0,_("Progressive")},
+  {1,_("Int. TFF")},
+  {2,_("Int. BFF")}
+};
+                      
+         diaElemBitrate bitrate(incoming,NULL);
+         diaElemMenu      widescreen(&(conf->widescreen),_("Aspect Ratio"),2,wideM);
+         diaElemMenu      matrix(&(conf->user_matrix),_("Matrices"),4,matrixM);
+         diaElemUInteger gop(&(conf->gop_size),_("Gop Size"),1,30);
+         diaElemUInteger maxb(&(conf->maxBitrate),_("Max Bitrate"),100,9000);
+
+uint32_t inter;
+          if(!conf->interlaced) inter=0;
+            else if(conf->bff) inter=2;
+                else inter=1;
+         diaElemMenu      interW(&inter,_("Interlacing"),3,interM);
+  
+      diaElem *elems[6]={&bitrate,&widescreen,&interW,&matrix,&gop,&maxb};
+    
+  if( diaFactoryRun("Mpeg2 Encoding",6,elems))
+  {
+    switch(inter)
+    {
+      case 0: conf->interlaced=0;conf->bff=0;break;
+      case 1: conf->interlaced=1;conf->bff=0;break;
+      case 2: conf->interlaced=1;conf->bff=1;break;
+      default: ADM_assert(0);
+    }
+    return 1;
+  }
+  return 0;
+}	
+// EOF
