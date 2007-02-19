685ab8d23b18863c5545bb8a7913adb6206deade
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-02-19 19:14:51 +0000

    temporary

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/FAC_filesel.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/FAC_filesel.cpp
@@ -24,31 +24,89 @@
 #include <QDialog>
 #include <QMessageBox>
 #include <QGridLayout>
-#include <QCheckBox>
+#include <QLineEdit>
+#include <QLabel>
+#include <QDialogButtonBox>
 
 #include "default.h"
 #include "ADM_commonUI/DIA_factory.h"
 #include "ADM_assert.h"
 
+extern const char *shortkey(const char *);
+
+
+class  ADM_Qfilesel : public QWidget
+{
+     Q_OBJECT
+    
+  signals:
+        
+        
+   public slots:
+        
+  public:
+        QLineEdit *edit;
+        QDialogButtonBox *button;
+        QLabel *text;
+            
+        ADM_Qfilesel(QWidget *z,const char *title,char *entry,QGridLayout *layout,int line) : QWidget(z) 
+        {
+          
+  
+          edit=new QLineEdit(entry,z);
+          
+          edit->show();
+          
+          button=new QDialogButtonBox(QDialogButtonBox::Open,Qt::Horizontal,z);
+          button->show();
+          
+        
+        
+          text=new QLabel( title,z);
+          text->setBuddy(edit);
+          layout->addWidget(text,line,0);
+          layout->addWidget(edit,line,1);
+          layout->addWidget(button,line,2);
+          //QObject::connect(&button, SIGNAL(accepted()), NULL, SLOT(accept())); 
+        }
+        ~ADM_Qfilesel() 
+            {
+                if(edit) delete edit;
+                if(button) delete button;
+                if(text) delete text;
+            };
+};
+
 
 diaElemFileRead::diaElemFileRead(char **filename,const char *toggleTitle,const char *tip)
   : diaElem(ELEM_FILE_READ)
 {
- 
+  param=(void *)filename;
+  paramTitle=shortkey(toggleTitle);
+  this->tip=tip;
 }
 
 diaElemFileRead::~diaElemFileRead()
 {
-  
+  if(paramTitle)
+    delete paramTitle;
 }
 void diaElemFileRead::setMe(void *dialog, void *opaque,uint32_t line)
 {
- 
+ QGridLayout *layout=(QGridLayout*) opaque;
   
+  ADM_Qfilesel *fs=new ADM_Qfilesel((QWidget *)dialog,paramTitle,*(char **)param,layout, line);
+  myWidget=(void *)fs; 
 }
 void diaElemFileRead::getMe(void)
 {
- 
+  
+  char **n=(char **)param;
+  if(*n) ADM_dealloc(*n);
+  
+  ADM_Qfilesel *fs=(ADM_Qfilesel *)myWidget;
+  QString s=(fs->edit)->text();
+  *n=ADM_strdup( s.toLatin1() );
 }
 
 //EOF
