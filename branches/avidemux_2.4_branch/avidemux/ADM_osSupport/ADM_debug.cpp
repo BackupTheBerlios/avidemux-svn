22c82e633c6f83da05b903fad3934054165fd685
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-05-06 09:34:46 +0000

    new adm_printf api

--- branches/avidemux_2.4_branch/avidemux/ADM_osSupport/ADM_debug.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_osSupport/ADM_debug.cpp
@@ -33,7 +33,11 @@
 
 // put here the module you want to be verbose (MODULE_xxx + MODULE_yyyy+  ....)
 #ifndef masked
-#define masked  (0*MODULE_UNPACKER+ 0*MODULE_LAVFORMAT) //+MODULE_3GP) //(MODULE_AUDIO_EDITOR) MODULE_OGM_AUDIO MODULE_REQUANT MODULE_CODEC
+#define masked  (MODULE_3GP*1+ 1*MODULE_MPEG) //+MODULE_3GP) //(MODULE_AUDIO_EDITOR) MODULE_OGM_AUDIO MODULE_REQUANT MODULE_CODEC
+#endif
+
+#ifndef thresholdLevel
+#define thresholdLevel ADM_PRINT_DEBUG
 #endif
 
 // If the entitty is in masked we actually print the string
@@ -59,6 +63,25 @@ extern "C"
 	}
   }
 
-}
 
+void indirect_printf_long(int level,const char *modname,int entity,const char *prf,  ...)
+{
+  static char print_buffer[1024];
+        if(level>thresholdLevel) return;
+  	if(1) //masked & entity)
+	{
+		va_list 	list;
+		va_start(list,	prf);
+		vsnprintf(print_buffer,1023,prf,list);
+		va_end(list);
+		print_buffer[1023]=0; // ensure the string is terminated
+		printf("[%s] LVL:%d %s",modname,level,print_buffer);
+
+	}
+	else
+  	{
+		return;
+	}
+}
+}
 //EOF
