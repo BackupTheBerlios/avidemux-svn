d49dbad6815f4fbd1e60a11944d8d07b9e6cf746
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-05-09 13:39:30 +0000

    requant uses dialogFactory

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialog/DIA_none.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialog/DIA_none.cpp
@@ -89,7 +89,6 @@ uint8_t DIA_getHue(Hue_Param *param, AVDMGenericVideoStream *in){return 0;}
 uint8_t DIA_getMPdelogo(MPDELOGO_PARAM *param,AVDMGenericVideoStream *in){return 0;}
 uint8_t DIA_getPartial(PARTIAL_CONFIG *param,AVDMGenericVideoStream *son,AVDMGenericVideoStream *previous){return 0;}
 uint8_t DIA_pipe(char **cmd,char **param){return 0;}
-uint8_t DIA_requant(COMPRES_PARAMS *incoming){return 0;}
 uint8_t  DIA_setUserMuxParam( int *mode, int *param, int *muxsize){return 0;}
 uint8_t  DIA_tdeint(TDEINT_PARAM *param){return 0;}
 uint8_t DIA_vobsub(vobSubParam *param){return 0;}
