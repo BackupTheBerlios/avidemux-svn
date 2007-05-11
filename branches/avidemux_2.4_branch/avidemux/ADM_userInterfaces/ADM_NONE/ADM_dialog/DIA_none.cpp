4a0babe302a1fdd5e83af5f453e812205d5e1e3d
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-05-11 08:37:41 +0000

    auto mode for qt+unlock resizer

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_NONE/ADM_dialog/DIA_none.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_NONE/ADM_dialog/DIA_none.cpp
@@ -128,7 +128,6 @@ uint8_t DIA_videoCodec (SelectCodecType * codec){return 0;}
 uint8_t DIA_getChromaShift( AVDMGenericVideoStream *instream,CHROMASHIFT_PARAM    *param ){ return 0;}
 uint8_t DIA_contrast(AVDMGenericVideoStream *astream,CONTRAST_PARAM *param)  { return 0;}
 uint8_t DIA_audioCodec( AUDIOENCODER *codec ) {return 0;}
-uint8_t DIA_resizeWiz(RESWIZ_FORMAT *format, RESWIZ_AR *source, RESWIZ_AR *destination) {return 0;}
 uint8_t DIA_dnr(uint32_t *llock,uint32_t *lthresh, uint32_t *clock,
 			uint32_t *cthresh, uint32_t *scene) {return 0;}
 			
