600efb337f98fe2d40b0823b8d11a2fe1012d114
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-05-29 09:34:41 +0000

    both compiling is not an error

--- branches/avidemux_2.4_branch/cmake/adm_compile.cmake
+++ branches/avidemux_2.4_branch/cmake/adm_compile.cmake
@@ -1,20 +1,20 @@
 # Small Macro to check head & lib and set output variable
 # all in one line, cmake is cool :)
-MACRO (ADM_COMPILE _file _cflags _lib _varToSet)
+MACRO (ADM_COMPILE _file _cflags _lib _varToSet _output)
 TRY_COMPILE(${_varToSet}
       ${CMAKE_BINARY_DIR}
       ${CMAKE_SOURCE_DIR}/cmake_compile_check/${_file}
       CMAKE_FLAGS -DLINK_LIBRARIES:STRING=${_lib}
       COMPILE_DEFINITIONS  ${_cflags}
       
-      OUTPUT_VARIABLE MYOUTPUT) 
+      OUTPUT_VARIABLE ${_output}) 
 #MESSAGE( ./compile.log "Compile:${MYOUTPUT}")
-ENDMACRO (ADM_COMPILE _file _cflags _varToSet)
+ENDMACRO (ADM_COMPILE _file _cflags _varToSet _output)
 #########################################################
 MACRO (ADM_COMPILE_WITH_WITHOUT _file _cflags _lib _varToSet)
 
-ADM_COMPILE( ${_file} ${_cflags} ${_lib} CWITH)
-ADM_COMPILE( ${_file} "" ${_lib}  CWITHOUT)
+ADM_COMPILE( ${_file} ${_cflags} ${_lib} CWITH logwith)
+ADM_COMPILE( ${_file} "" ${_lib}  CWITHOUT logwithout)
 
 if(CWITH AND NOT CWITHOUT)
   SET(${_varToSet}  1)
@@ -23,7 +23,13 @@ else(CWITH AND NOT CWITHOUT)
   if(NOT CWITH AND CWITHOUT)
     MESSAGE (STATUS "<${_cflags}> Not needed")
   else(NOT CWITH AND CWITHOUT)
-    MESSAGE(FATAL "Inconsistent compiler output with:${CWITH}, without:${CWITHOUT}")
+    if(NOT CWIDTH AND NOT CWITHOUT)
+      MESSAGE(STATUS "Inconsistent compiler output with:${CWITH}, without:${CWITHOUT}")
+      MESSAGE(STATUS "**********************************")
+      MESSAGE(STATUS "${logwith}")
+      MESSAGE(STATUS "**********************************")
+      MESSAGE(STATUS "${logwithout}")
+    endif(NOT CWIDTH AND NOT CWITHOUT)
   endif(NOT CWITH AND CWITHOUT)
 endif(CWITH AND NOT CWITHOUT)
 ENDMACRO (ADM_COMPILE_WITH_WITHOUT _file _cflags _varToSet)
