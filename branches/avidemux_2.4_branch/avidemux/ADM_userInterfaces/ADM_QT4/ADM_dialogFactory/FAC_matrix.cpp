dc86f794777386a0270d85f2e4d0e8b4a1b30c82
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2007-08-21 18:53:32 +0000

    [QT] Matrix diaElem for QT

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/FAC_matrix.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialogFactory/FAC_matrix.cpp
@@ -38,20 +38,80 @@ extern const char *shortkey(const char *);
 diaElemMatrix::diaElemMatrix(uint8_t *trix,const char *toggleTitle, uint32_t trixSize,const char *tip)
   : diaElem(ELEM_MATRIX)
 {
+	
+	  param=(void *)trix;
+	  paramTitle=shortkey(toggleTitle);
+	  _matrix=new uint8_t[trixSize*trixSize];
+	   _matrixSize=trixSize;
+	   memcpy(_matrix,trix,trixSize*trixSize);
+	  this->tip=tip;
+	
 }
 
 diaElemMatrix::~diaElemMatrix()
 {
+	 if(_matrix) delete [] _matrix;
+	  _matrix=NULL;
+	  if(myWidget)
+	  {
+		  QSpinBox **arrayWidget=( QSpinBox **)myWidget;
+		  delete [] arrayWidget;
+		  myWidget=NULL;
+	  }
+	  if(paramTitle)
+	      delete paramTitle;
+	  paramTitle=NULL;
 }
 void diaElemMatrix::setMe(void *dialog, void *opaque,uint32_t line)
 {
+	 QSpinBox **box=new QSpinBox*[_matrixSize*_matrixSize];
+	  QGridLayout *layout=(QGridLayout*) opaque;
+	 myWidget=(void *)box; 
 
+
+	  
+	  
+	 
+	 QLabel *text=new QLabel( this->paramTitle,(QWidget *)dialog);
+	 layout->addWidget(text,line,0);
+	 
+	 QGridLayout *layout2=new QGridLayout((QWidget *)dialog);
+	  layout->addLayout(layout2,line,1);
+	  /*layout->setMargin(0);
+	  layout->setSpacing(0);*/
+	 
+
+	 for(int y=0;y<_matrixSize*_matrixSize;y++)
+	 			  {
+	 				  box[y]=new QSpinBox((QWidget *)dialog);
+	 				  box[y]->setMinimum(0);
+	 				  box[y]->setMaximum(255);
+	 				  box[y]->setValue(_matrix[y]);
+	 				 layout2->addWidget(box[y],y/_matrixSize,y%_matrixSize);
+	 			  }
+	 myWidget =(void *)box;
 }
 void diaElemMatrix::getMe(void)
 {
-  
+	 QSpinBox **box=( QSpinBox **)myWidget;
+	 ADM_assert(box);
+	for(int y=0;y<_matrixSize*_matrixSize;y++)
+		 			  {
+		 				  _matrix[y]=box[y]->value();
+		 			  }
+	memcpy(param,_matrix,_matrixSize*_matrixSize);
 }
 void diaElemMatrix::enable(uint32_t onoff)
 {
+	QSpinBox **arrayWidget=( QSpinBox **)myWidget;
+			 
+			 ADM_assert(arrayWidget);
+			 int index=0;
+			  for(int y=0;y<_matrixSize*_matrixSize;y++)
+			  {
+				  if(onoff) arrayWidget[y]->setEnabled(1);
+				  else arrayWidget[y]->setEnabled(0);
+				  
+			  }
 }
 
