a74d928e1c0965c2dc5ccb958ca63e9df8cf8716
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2008-03-04 10:19:08 +0000

    [CMake] test GCC for OS

--- branches/avidemux_2.5_branch_gruntster/cmake/admDetermineSystem.cmake
+++ branches/avidemux_2.5_branch_gruntster/cmake/admDetermineSystem.cmake
@@ -6,7 +6,37 @@
 #  ADM_CPU_X86_64        - x86-64 CPU architecture was detected
 #  CMAKE_WORDS_BIGENDIAN - big endian CPU detected
 
-INCLUDE(CMakeDetermineSystem)
+MACRO (PERFORM_SYSTEM_TEST testFile testName testSupportedVarName)
+	IF (${ARGC} EQUAL 4)
+		SET (testFlags ${ARGV3})
+	ELSE (${ARGC} EQUAL 4)
+		SET (testFlags "")
+	ENDIF (${ARGC} EQUAL 4)
+
+	ADM_COMPILE(${testFile} "${testFlags}" "" "" ${testSupportedVarName} outputTest)
+
+	IF (${testSupportedVarName})
+		SET(testResult "Yes")
+	ELSE (${testSupportedVarName})
+		SET(testResult "No")
+	ENDIF (${testSupportedVarName})
+
+	IF (${testSupportedVarName} OR VERBOSE)
+		MESSAGE(STATUS "Check if GCC is ${testName} - ${testResult}")
+	ENDIF (${testSupportedVarName} OR VERBOSE)
+
+	IF (NOT ${testSupportedVarName} AND VERBOSE)
+		MESSAGE("Error Message: ${outputTest}")
+	ENDIF (NOT ${testSupportedVarName} AND VERBOSE)
+ENDMACRO (PERFORM_SYSTEM_TEST)
+
+# clear previous settings
+SET(APPLE)
+SET(CYGWIN)
+SET(MINGW)
+SET(UNIX)
+SET(WIN32)
+
 INCLUDE(TestBigEndian)
 
 MESSAGE(STATUS "Checking GCC support")
@@ -15,53 +45,61 @@ MESSAGE(STATUS "********************")
 ########################################
 # Check OS support
 ########################################
-ADM_COMPILE(bsd_check.cpp "" "" "" BSD_SUPPORTED outputBsdTest)
+PERFORM_SYSTEM_TEST(os_unix_check.cpp "Unix" UNIX_SUPPORTED)
 
-IF (BSD_SUPPORTED)
-	SET(ADM_BSD_FAMILY 1)
+IF (UNIX_SUPPORTED)
+	SET(UNIX 1)
 
-	MESSAGE(STATUS "Check if GCC is BSD family - Yes")
-ELSE (BSD_SUPPORTED)
-	MESSAGE(STATUS "Check if GCC is BSD family - No")
+	PERFORM_SYSTEM_TEST(os_bsd_check.cpp "BSD family" BSD_SUPPORTED)
 
-	IF (VERBOSE)
-		MESSAGE("Error Message: ${outputBsdTest}")
-	ENDIF (VERBOSE)
-ENDIF (BSD_SUPPORTED)
+	IF (BSD_SUPPORTED)
+		SET(ADM_BSD_FAMILY 1)
+		PERFORM_SYSTEM_TEST(os_apple_check.cpp "Apple" APPLE_SUPPORTED)
+
+		IF (APPLE_SUPPORTED)
+			SET(APPLE 1)
+		ENDIF (APPLE_SUPPORTED)
+	ELSE (BSD_SUPPORTED)
+		PERFORM_SYSTEM_TEST(os_cygwin_check.cpp "Cygwin" CYGWIN_SUPPORTED)
+
+		IF (CYGWIN_SUPPORTED)
+			SET(CYGWIN 1)
+
+			PERFORM_SYSTEM_TEST(os_win32_check.cpp "Win32" WIN32_SUPPORTED "-mwin32")
+
+			IF (WIN32_SUPPORTED)
+				SET(WIN32 1)
+			ENDIF (WIN32_SUPPORTED)
+		ENDIF (CYGWIN_SUPPORTED)
+	ENDIF (BSD_SUPPORTED)
+ELSE (UNIX_SUPPORTED)
+	PERFORM_SYSTEM_TEST(os_win32_check.cpp "Win32" WIN32_SUPPORTED)
+
+	IF (WIN32_SUPPORTED)
+		SET(WIN32 1)
+
+		PERFORM_SYSTEM_TEST(os_mingw_check.cpp "MinGW" MINGW_SUPPORTED)
+
+		IF (MINGW_SUPPORTED)
+			SET(MINGW 1)
+		ENDIF (MINGW_SUPPORTED)
+	ENDIF (WIN32_SUPPORTED)
+ENDIF (UNIX_SUPPORTED)
 
 ########################################
 # Check CPU support
 ########################################
-# x86_64
-ADM_COMPILE(cpu_x86-64_check.cpp "" "" "" X86_64_SUPPORTED outputx86_64Test)
+PERFORM_SYSTEM_TEST(cpu_x86-64_check.cpp "x86-64" X86_64_SUPPORTED)
 
 IF (X86_64_SUPPORTED)
 	SET(ADM_CPU_X86_64 1)
-
-	MESSAGE(STATUS "Check if GCC is x86 64-bit - Yes")
 ELSE (X86_64_SUPPORTED)
-	MESSAGE(STATUS "Check if GCC is x86 64-bit - No")
-
-	IF (VERBOSE)
-		MESSAGE("Error Message: ${outputx86_64Test}")
-	ENDIF (VERBOSE)
-
-	# x86
-	ADM_COMPILE(cpu_x86_check.cpp "" "" "" X86_SUPPORTED outputX86Test)
+	PERFORM_SYSTEM_TEST(cpu_x86_check.cpp "x86" X86_SUPPORTED)
 
 	IF (X86_SUPPORTED)
 		SET(ADM_CPU_X86 1)
-
-		MESSAGE(STATUS "Check if GCC is x86 32-bit - Yes")
 	ELSE (X86_SUPPORTED)
-		MESSAGE(STATUS "Check if GCC is x86 32-bit - No")
-
-		IF (VERBOSE)
-			MESSAGE("Error Message: ${outputX86Test}")
-		ENDIF (VERBOSE)
-
-		# PowerPC
-		ADM_COMPILE(cpu_ppc_check.cpp "" "" "" POWERPC_SUPPORTED outputPowerPcTest)
+		PERFORM_SYSTEM_TEST(cpu_ppc_check.cpp "PowerPC" POWERPC_SUPPORTED)
 
 		IF (POWERPC_SUPPORTED)
 			SET(ADM_CPU_PPC 1)
@@ -72,25 +110,11 @@ ELSE (X86_64_SUPPORTED)
 				SET(ADM_ALTIVEC_FLAGS "${ADM_ALTIVEC_FLAGS} -faltivec")
 			ENDIF (APPLE)
 
-			MESSAGE(STATUS "Check if GCC is PowerPC - Yes")	
-			ADM_COMPILE(cpu_altivec_check.cpp "${ADM_ALTIVEC_FLAGS}" "" "" ALTIVEC_SUPPORTED outputAltivecTest)
+			PERFORM_SYSTEM_TEST(cpu_altivec_check.cpp "AltiVec" ALTIVEC_SUPPORTED)
 
 			IF (ALTIVEC_SUPPORTED)
 				SET(ADM_CPU_ALTIVEC 1)
-				MESSAGE(STATUS "Check if GCC is AltiVec - Yes")
-			ELSE (ALTIVEC_SUPPORTED)
-				MESSAGE(STATUS "Check if GCC is AltiVec - No")
-
-				IF (VERBOSE)
-					MESSAGE("Error Message: ${outputAltivecTest}")
-				ENDIF (VERBOSE)
 			ENDIF (ALTIVEC_SUPPORTED)
-		ELSE (POWERPC_SUPPORTED)
-			MESSAGE(STATUS "Check if GCC is PowerPC - No")
-
-			IF (VERBOSE)
-				MESSAGE("Error Message: ${outputPowerPcTest}")
-			ENDIF (VERBOSE)
 		ENDIF (POWERPC_SUPPORTED)
 	ENDIF (X86_SUPPORTED)
 ENDIF (X86_64_SUPPORTED)
