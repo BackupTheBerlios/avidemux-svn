1441950ea4edc8068f43bba2c4d05b0ff2545ac2
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2008-03-09 18:05:14 +0000

    [AudioFiler] Install the plugins

--- branches/avidemux_2.5_branch_mean_merged/cmake/ad_plugin.cmake
+++ branches/avidemux_2.5_branch_mean_merged/cmake/ad_plugin.cmake
@@ -1,6 +1,6 @@
 MACRO(ADM_audio_plugin  _lib)
-ADD_TARGET_CFLAGS(${_lib} "-I${CMAKE_BINARY_DIR}/config/cli")
-ADD_TARGET_CFLAGS(${_lib} "-I${CMAKE_SOURCE_DIR}/avidemux/ADM_audiocodec")
-INSTALL(FILES ${_lib} DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/ADM_plugins/audioDecoder/")
-MACRO(ADM_audio_plugin)
+        ADD_TARGET_CFLAGS(${_lib} "-I${CMAKE_BINARY_DIR}/config/cli")
+        ADD_TARGET_CFLAGS(${_lib} "-I${CMAKE_SOURCE_DIR}/avidemux/ADM_audiocodec")
+        INSTALL(TARGETS ${_lib} LIBRARY DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/ADM_plugins/audioDecoder/")
+ENDMACRO(ADM_audio_plugin)
 
