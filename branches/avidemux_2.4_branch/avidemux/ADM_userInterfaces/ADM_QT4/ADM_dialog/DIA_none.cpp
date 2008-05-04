6c0fccb3e9ffd44cd9a8b9dc7ecd29a9208cff65
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2008-05-04 18:37:37 +0000

    [Qt4] about window

--- branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialog/DIA_none.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_userInterfaces/ADM_QT4/ADM_dialog/DIA_none.cpp
@@ -45,7 +45,6 @@ uint8_t DIA_videoCodec (SelectCodecType * codec){return 0;}
 uint8_t DIA_audioCodec( AUDIOENCODER *codec ) {return 0;}
 uint8_t DIA_quota(char *) {return 0;}
 uint8_t DIA_RecentFiles( char **name ){return 0;}
-uint8_t DIA_about( void ){return 0;}
 void    DIA_Calculator(uint32_t *sizeInMeg, uint32_t *avgBitrate ){}
 uint8_t initFileSelector(void){return 0;}
 uint8_t initGUI( void ) {return 1;}
