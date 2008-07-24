f1aafcde60e3141459be8699a433d3173da3ac29
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2008-07-24 11:49:40 +0000

    [ECMA] implement include() for MinGW

--- branches/avidemux_2.4_branch/avidemux/ADM_script/ADM_JSFunctions.cpp
+++ branches/avidemux_2.4_branch/avidemux/ADM_script/ADM_JSFunctions.cpp
@@ -420,14 +420,6 @@ JSBool systemExecute(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsva
 	return JS_TRUE;
 }// end systemExecute
 
-#ifdef ADM_WIN32
-
-JSBool systemInclude(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
-{
-		return JS_FALSE;
-}
-#else
-
 JSBool systemInclude(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {// begin systemInclude
 	// default return value
@@ -442,7 +434,12 @@ JSBool systemInclude(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsva
 	// make sure we haven't included this already to avoid a recursive
 	// dependency loop
 	char *pTempStr = new char[PATH_MAX+1];
+
+#ifdef __MINGW32__
+	if(_fullpath(pTempStr,pIncludeFile,PATH_MAX) == NULL)
+#else
 	if(realpath(pIncludeFile,pTempStr) == NULL)
+#endif
 	{// begin can't resolve path
 		JS_ReportError(cx, "include() can't resolve the path of \"%s\".", pIncludeFile);
 		return JS_FALSE;
@@ -480,7 +477,6 @@ JSBool systemInclude(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsva
 	}// end error including
 	return JS_TRUE;
 }// end systemInclude
-#endif
 
 /********************** Extract Path from a filename **************/
 JSBool pathOnly(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
