ffbac8fbe69c35d3a8a1c1953ae205045dedb468
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-11 07:34:42 +0000

    [Build] Bootstrap script

--- branches/avidemux_2.5_branch_gruntster/bootStrap.sh
+++ branches/avidemux_2.5_branch_gruntster/bootStrap.sh
@@ -12,8 +12,17 @@ echo "* Building Main *"
 rm -Rf buildMain
 mkdir -p buildMain
 cd buildMain
-cmake .. || fail
-make || fail
-echp "*  Main Ok*"
-echp "*  All Done  *"
+cmake .. || fail cmake
+make || fail make_main
+sudo make install | fail install_main
+echo "*  Main Ok*"
+cd $TOP
+echo "* Making plugins *"
+rm -Rf buildPlugins
+mkdir -p buildPlugins
+cd buildPlugins
+cmake -DAVIDEMUX_INSTALL_PREFIX=/usr/  -DAVIDEMUX_SOURCE_DIR=$TOP/  -DAVIDEMUX_CORECONFIG_DIR=$TOP/buildMain/config ../plugins || fail cmake_plugins
+make || fail make_plugins
+echo "*  All Done  *"
+
 
