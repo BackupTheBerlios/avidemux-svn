6d87cda2cc9d67f4c568ea5521dc398e1641f481
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-11 18:51:59 +0000

    [bootStrap] Fix install path, have to specify twice

--- branches/avidemux_2.5_branch_gruntster/bootStrap.sh
+++ branches/avidemux_2.5_branch_gruntster/bootStrap.sh
@@ -5,14 +5,14 @@ fail()
         exit 1
         cd $TOP
 }
-
+export PREFIX=/usr
 export TOP=$PWD
 echo "* Avidemux simple build script *"
 echo "* Building Main *"
 rm -Rf buildMain
 mkdir -p buildMain
 cd buildMain
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DAVIDEMUX_INSTALL_PREFIX=/usr/ .. || fail cmake
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DAVIDEMUX_INSTALL_PREFIX=$PREFIX .. || fail cmake
 make || fail make_main
 sudo make install | fail install_main
 echo "*  Main Ok*"
@@ -21,7 +21,7 @@ echo "* Making plugins *"
 rm -Rf buildPlugins
 mkdir -p buildPlugins
 cd buildPlugins
-cmake -DAVIDEMUX_INSTALL_PREFIX=/usr/  -DAVIDEMUX_SOURCE_DIR=$TOP/  -DAVIDEMUX_CORECONFIG_DIR=$TOP/buildMain/config ../plugins || fail cmake_plugins
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DAVIDEMUX_INSTALL_PREFIX=$PREFIX  -DAVIDEMUX_SOURCE_DIR=$TOP/  -DAVIDEMUX_CORECONFIG_DIR=$TOP/buildMain/config ../plugins || fail cmake_plugins
 make || fail make_plugins
 echo "*  All Done  *"
 
