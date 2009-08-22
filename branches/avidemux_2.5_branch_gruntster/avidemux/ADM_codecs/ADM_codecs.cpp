8f9803d59c25c71dd9a948169ea12f7b5b7b89b1
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-08-22 06:59:07 +0000

    [Codec] WMVA==VC1 video codec

--- branches/avidemux_2.5_branch_gruntster/avidemux/ADM_codecs/ADM_codecs.cpp
+++ branches/avidemux_2.5_branch_gruntster/avidemux/ADM_codecs/ADM_codecs.cpp
@@ -295,7 +295,8 @@ getDecoder (uint32_t fcc, uint32_t w, uint32_t h, uint32_t extraLen,
       return (decoders *) (new decoderFFWMV3 (w, h, extraLen, extraData));
     }
 
-    if (fourCC::check (fcc, (uint8_t *) "WVC1"))
+    if (fourCC::check (fcc, (uint8_t *) "WVC1") ||
+                fourCC::check (fcc, (uint8_t *) "WMVA")  )
     {
 
       return (decoders *) (new decoderFFVC1 (w, h, extraLen, extraData));
