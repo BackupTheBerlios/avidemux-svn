29c685311f4b8ce658bbce8e003f9b501ce43c0b
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-12-19 20:26:13 +0000

    [Win32] update build scripts to support 64-bit build

--- branches/avidemux_2.5_branch_gruntster/platforms/windows/build_scripts/avidemux/1a. Prepare Build.bat
+++ branches/avidemux_2.5_branch_gruntster/platforms/windows/build_scripts/avidemux/1a. Prepare Build.bat
@@ -6,18 +6,18 @@ call "Set Avidemux Environment Variables"
 if errorlevel 1 goto error
 
 cd "%sourceDir%"
-echo Cleaning build directories
-rmdir /s /q build 2> NUL
+echo Cleaning build directory (%CD%\%BuildFolder%)
+rmdir /s /q %buildFolder% 2> NUL
 
 cd "%sourceDir%\plugins"
-rmdir /s /q build 2> NUL
+rmdir /s /q %buildFolder% 2> NUL
 
-if exist build goto removalFailure
+if exist %buildFolder% goto removalFailure
 cd "%sourceDir%"
-if exist build goto removalFailure
+if exist %buildFolder% goto removalFailure
 
-mkdir build
-mkdir plugins\build
+mkdir %buildFolder%
+mkdir plugins\%buildFolder%
 
 echo Performing Subversion update
 svn up .
@@ -33,6 +33,6 @@ echo Aborting.
 
 :error
 set ERRORLEVEL=1
+pause
 
 :end
-pause
