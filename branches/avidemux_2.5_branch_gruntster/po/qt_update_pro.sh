8d9fe8c63c97c592c1942cb341627e46a4e66e78
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-05-25 21:03:11 +0000

    [i18n] include plugins in generation of translation sources

--- branches/avidemux_2.5_branch_gruntster/po/qt_update_pro.sh
+++ branches/avidemux_2.5_branch_gruntster/po/qt_update_pro.sh
@@ -6,16 +6,19 @@
 echo "Finding HEADERS..."
 echo "HEADERS = \\" > avidemux.pro
 find ../avidemux -iname '*.h' -not -iwholename '*ADM_GTK*' -not -iwholename '*ADM_NONE*' -printf "%p \\\ \n" >> avidemux.pro
+find ../plugins -iname '*.h' -not -iwholename '*gtk*' -printf "%p \\\ \n" >> avidemux.pro
 echo "" >> avidemux.pro
 
 echo "Finding SOURCES..."
 echo "SOURCES = \\" >> avidemux.pro
 find ../avidemux -iname '*.cpp' -not -iwholename '*ADM_GTK*' -not -iwholename '*ADM_NONE*' -printf "%p \\\ \n" >> avidemux.pro
+find ../plugins -iname '*.cpp' -not -iwholename '*gtk*' -printf "%p \\\ \n" >> avidemux.pro
 echo "" >> avidemux.pro
 
 echo "Finding FORMS..."
 echo "FORMS = \\" >> avidemux.pro
 find ../avidemux -iname '*.ui' -not -iwholename '*ADM_GTK*' -not -iwholename '*ADM_NONE*' -printf "%p \\\ \n" >> avidemux.pro
+find ../plugins -iname '*.ui' -not -iwholename '*gtk*' -printf "%p \\\ \n" >> avidemux.pro
 echo "" >> avidemux.pro
 
 echo "Finding TRANSLATIONS..."
