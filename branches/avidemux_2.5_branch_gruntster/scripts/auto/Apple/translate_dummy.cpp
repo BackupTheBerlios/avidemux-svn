2557dfad0af03b183c33090d3f7ec2547af901b8
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-06-10 18:48:38 +0000

    [auto] drop dead auto wizards

--- branches/avidemux_2.5_branch_gruntster/scripts/auto/Apple/translate_dummy.cpp
+++
@@ -1,7 +0,0 @@
-MainWindow::TranslateMe
-{
-	QT_TR_NOOP("Apple");
-	QT_TR_NOOP("Apple iPhone");
-	QT_TR_NOOP("Apple iPod 5.5G");
-	QT_TR_NOOP("Apple iPod");
-}
--- branches/avidemux_2.5_branch_gruntster/scripts/auto/translate_dummy.cpp
+++
@@ -1,5 +0,0 @@
-MainWindow::TranslateMe
-{
-	QT_TR_NOOP("Microsoft Zune");
-	QT_TR_NOOP("Sony PlayStation Portable");
-}
