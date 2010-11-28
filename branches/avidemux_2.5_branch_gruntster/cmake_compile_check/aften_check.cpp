876aa55436758e7f6fe66216556d391470971966
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-11-28 09:23:33 +0000

    [aften] add support for git version of Aften

--- branches/avidemux_2.5_branch_gruntster/cmake_compile_check/aften_check.cpp
+++ branches/avidemux_2.5_branch_gruntster/cmake_compile_check/aften_check.cpp
@@ -5,13 +5,16 @@
 int main(int argc, char **argv)
 {
 	const char* aftenVersion = aften_get_version();
-//        fprintf(stderr,"Aften version = %s\n",aftenVersion);
+	//fprintf(stderr, "Aften version = %s\n", aftenVersion);
+
 	if (strcmp(aftenVersion, "0.07") == 0)
 		return 7;
 	else if (strcmp(aftenVersion, "0.0.8") == 0)
 		return 8;
-        else if (strcmp(aftenVersion, "SVN") == 0)
-                return 99;
+	else if (strcmp(aftenVersion, "SVN") == 0)
+		return 99;
+	else if (strncmp(aftenVersion, "git", 3) == 0)
+		return 99;
 
-	return 0;
+	return -1;
 }
