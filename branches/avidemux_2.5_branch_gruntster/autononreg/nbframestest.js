f9179f3f21827eea90170520507b5dd44cd7c94b
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-09-11 16:43:00 +0000

    [js] update framecount test

--- branches/avidemux_2.5_branch_gruntster/autononreg/nbframestest.js
+++ branches/avidemux_2.5_branch_gruntster/autononreg/nbframestest.js
@@ -1,13 +1,13 @@
 //AD  <- These first 4 characters need to be the first 4 characters to identify the ECMAScript file to Avidemux
 var app = new Avidemux();
-var file="/work/samples/2mn.avi";
+var file="/work/samples/avi/2mn.avi";
 var nbf;
 /* Load file
 */
 app.load(file);
 /* Test get nbf 
 */
-nbf=app.video.getNbFrames();
+nbf=app.video.frameCount;
 	displayInfo("Nb frames:"+nbf);
 
 /* End of test
