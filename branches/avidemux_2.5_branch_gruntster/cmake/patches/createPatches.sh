1caf44dccdf509ad687c2b6bc8ca14d1c6407133
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-12-02 18:28:43 +0000

    [ffmpeg] upgrade ffmpeg to 0.8.7

--- branches/avidemux_2.5_branch_gruntster/cmake/patches/createPatches.sh
+++ branches/avidemux_2.5_branch_gruntster/cmake/patches/createPatches.sh
@@ -2,7 +2,7 @@
 
 export curDir=$PWD
 export ffmpegPath=../../avidemux/ADM_libraries/ffmpeg
-export origFfmpegPath=../ffmpeg-0.8.6
+export origFfmpegPath=../ffmpeg-0.8.7
 
 function updatePatch {
 	#cd $ffmpegPath
