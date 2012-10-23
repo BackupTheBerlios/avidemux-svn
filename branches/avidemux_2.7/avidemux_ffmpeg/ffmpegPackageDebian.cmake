83586d5110a0f2d045aecc9dd473b871174fce45
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2012-10-23 08:22:54 +0000

    [cmake] tweak packaging

--- branches/avidemux_2.7/avidemux_ffmpeg/ffmpegPackageDebian.cmake
+++ branches/avidemux_2.7/avidemux_ffmpeg/ffmpegPackageDebian.cmake
@@ -1,19 +1,19 @@
-#############################
+##############################
 # DEBIAN
 ##############################
 include(debianArch)
 SET(CPACK_SET_DESTDIR "ON")
-SET(CPACK_DEBIAN_PACKAGE_NAME "avidemux3-gtk")
+SET(CPACK_DEBIAN_PACKAGE_NAME "avidemux3-ffmpeg")
 SET (CPACK_GENERATOR "DEB")
 # ARCH
 SET_DEBIAN_ARCH()
 # Mandatory
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "mean")
-SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editor,main program gtk version ")
+SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editor, ffmpeg libraries")
 
 # Build our deps list
-# Build deps SET(DEPS "debhelper (>> 4), libfreetype6-dev, libxml2-dev,  libxv-dev, dpatch , cmake, desktop-file-utils")
-SET(DEPS "libc6 (>=2.4),libglib2.0-0 (>=2.14.0),libstdc++6 (>=4.2.1),libx11-6,  libxv1, zlib1g (>=1:1.1.4), avidemux3-core (>=2.7.0)")
+SET(DEPS "libc6 (>=2.4), libstdc++6 (>=4.2.1)")
+
 # Add optional DEPS here
 SET(CPACK_DEBIAN_PACKAGE_DEPENDS "${DEPS}")
 #
@@ -23,9 +23,10 @@ SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 SET(CPACK_PACKAGE_VERSION_MAJOR "2")
 SET(CPACK_PACKAGE_VERSION_MINOR "7")
 SET(CPACK_PACKAGE_VERSION_PATCH "0")
+
 SET(CPACK_PACKAGE_VERSION_PATCH "0-${ADM_SUBVERSION}")
 #
-SET(CPACK_PACKAGE_NAME "avidemux3-gtk")
+SET(CPACK_PACKAGE_NAME "avidemux3-ffmpeg")
 #
 
 include(CPack)
